650e7a7e06beb8c170211cd043c84e6db6faf6b1
izhengly <820359064@qq.com>
2016-04-05 15:42:36 +0800

    删除一些重复的文件

--- titan/src/main/webapp/js/controllers/ap/cooperationHospitalPriceCtrl.js
+++
@@ -1,8 +0,0 @@
-﻿angular.module('controllers', ['ionic']).controller('cooperationHospitalPriceCtrl', [
-    '$scope','$state','$stateParams','GetCooperationHospitalInfo',
-        function ($scope,$state,$stateParams,GetCooperationHospitalInfo) {
-            $scope.haveOrder = false;
-            $scope.$on('$ionicView.enter',function() {
-
-            })
-    }])
