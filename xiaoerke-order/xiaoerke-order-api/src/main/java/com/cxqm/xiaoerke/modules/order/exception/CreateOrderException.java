fcfd9e313a005dd38c96f1bff877d74b55647b6b
王保伟 <565871712@qq.com>
2016-04-05 14:47:11 +0800

    创建异常类

---
+++ xiaoerke-order/xiaoerke-order-api/src/main/java/com/cxqm/xiaoerke/modules/order/exception/CreateOrderException.java
@@ -0,0 +1,18 @@
+package com.cxqm.xiaoerke.modules.order.exception;
+
+/**
+ * Created by wangbaowei on 16/3/31.
+ */
+public class CreateOrderException extends Exception{
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
+    public CreateOrderException() {
+    }
+
+    public CreateOrderException(String message) {
+        super(message);
+    }
+}
