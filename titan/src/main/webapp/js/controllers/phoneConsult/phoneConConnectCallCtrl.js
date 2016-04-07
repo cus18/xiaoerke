d1b73a8d790a60d9bb36efc386a606d684662db4
王保伟 <565871712@qq.com>
2016-04-07 14:18:29 +0800

    完善定时器

--- titan/src/main/webapp/js/controllers/phoneConsult/phoneConConnectCallCtrl.js
+++ titan/src/main/webapp/js/controllers/phoneConsult/phoneConConnectCallCtrl.js
@@ -1,9 +1,11 @@
 ﻿angular.module('controllers', ['ionic']).controller('phoneConConnectCallCtrl', [
-        '$scope','$state',
-        function ($scope,$state) {
+        '$scope','$state','$stateParams',
+        function ($scope,$state,$stateParams) {
             $scope.title = "连接通话";
             $scope.pageLoading =false;
-
+            $scope.phone = $stateParams.phone
+            $scope.doctorName = $stateParams.doctorName
+            $scope.doctorId = $stateParams.doctorId
             $scope.$on('$ionicView.enter', function(){
 
 
