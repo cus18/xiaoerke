1bb7161653e4f873c10c616b76a0b380bd501bd4
王保伟 <565871712@qq.com>
2016-04-07 11:45:42 +0800

    重新连接

--- titan/src/main/webapp/js/controllers/phoneConsult/phoneConReconnectionCtrl.js
+++ titan/src/main/webapp/js/controllers/phoneConsult/phoneConReconnectionCtrl.js
@@ -1,15 +1,22 @@
 ﻿angular.module('controllers', ['ionic']).controller('phoneConReconnectionCtrl', [
-        '$scope','$state',
-        function ($scope,$state) {
+        '$scope','$state','$stateParams','PhoneConsultRegisterInfo',
+        function ($scope,$state,$stateParams,PhoneConsultRegisterInfo) {
             $scope.title = "重新连接";
             $scope.pageLoading =false;
 
-            $scope.$on('$ionicView.enter', function(){
 
-
+            PhoneConsultRegisterInfo.get({phoneConsultaServiceId:$stateParams.consultPhoneServiceId},function(data){
+                $scope.orderInfo = data
+                $scope.position = (data.position1 == ""?"":data.position1+",") + data.position2
+                $scope.surplusTime =moment(data.surplusTime).format("mm:ss")
             });
+            //
+            //$scope.$on('$ionicView.enter', function(){
+            //
+            //
+            //});
 
             $scope.reconnection = function(){
-                $state.go("phoneConConnectCall");
+                $state.go("phoneConConnectCall",{phoneConsultaServiceId:$stateParams.consultPhoneServiceId})
             }
     }]);
