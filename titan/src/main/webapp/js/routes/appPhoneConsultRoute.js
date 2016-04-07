38f96eec3e039220aea8244519b6b08caf9ef606
王保伟 <565871712@qq.com>
2016-04-07 11:56:24 +0800

    重新连接页面

--- titan/src/main/webapp/js/routes/appPhoneConsultRoute.js
+++ titan/src/main/webapp/js/routes/appPhoneConsultRoute.js
@@ -273,7 +273,7 @@ define(['appPhoneConsult'], function(app){
                         }
                     })
                     .state('phoneConConnectCall', {
-                        url: '/phoneConConnectCall/:phoneConsultaServiceId',
+                        url: '/phoneConConnectCall/:phoneConsultaServiceId,:doctorName,:phone',
                         templateProvider: function() { return lazyDeferred.promise; },
                         controller: 'phoneConConnectCallCtrl',
                         resolve: {
