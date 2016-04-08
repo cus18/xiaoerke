5aed3900e190b06adff3b32381d4bbd704d57224
王保伟 <565871712@qq.com>
2016-04-08 11:54:40 +0800

    订单表增加字段

--- xiaoerke-consult/xiaoerke-consult-service/src/main/java/com/cxqm/xiaoerke/modules/consult/service/impl/ConsultPhoneServiceImpl.java
+++ xiaoerke-consult/xiaoerke-consult-service/src/main/java/com/cxqm/xiaoerke/modules/consult/service/impl/ConsultPhoneServiceImpl.java
@@ -131,7 +131,8 @@ public class ConsultPhoneServiceImpl implements ConsultPhoneService {
         consultPhoneRecordDao.insertSelective(vo);
         //挂机请求 ,根据剩余时长给用户推送消息,让用户在意外挂断的情况下可以再次接通 判断订单状态是否已推送过 此消息
         Map<String,Object> phonepatientInfo = consultPhonePatientService.getPatientRegisterInfo(Integer.parseInt(userData));
-        Integer serviceLength = (Integer)phonepatientInfo.get("server_length");
+
+        Integer serviceLength = (Integer)phonepatientInfo.get("server_length")*60*1000;
         String talkDuration = vo.getTalkduration();
 
         ConsultPhoneRegisterServiceVo consultPhonevo = new ConsultPhoneRegisterServiceVo();
