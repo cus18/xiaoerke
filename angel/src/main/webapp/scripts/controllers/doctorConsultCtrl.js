37c136c74ccf82e1a4b3ebd919c54daceb578d4c
lipangpangz <270255061>
2016-04-12 15:47:28 +0800

    0412

--- angel/src/main/webapp/scripts/controllers/doctorConsultCtrl.js
+++ angel/src/main/webapp/scripts/controllers/doctorConsultCtrl.js
@@ -11,6 +11,8 @@ angular.module('controllers', ['luegg.directives'])
             var currDate = new moment().format("YYYY-MM-DD");
             getTodayRankingList.save({"rankDate": currDate}, function (data) {
                 console.log("getTodayRankingList", data);
+                $scope.info.rankListlength = data.rankListValue.length;
+                console.log("$scope.info.rankListlength", data.rankListValue.length);
                 if (data.rankListValue.length == 0) {
                     $scope.lockScroll = "false";
                     console.log("当前没有排名");
@@ -24,14 +26,15 @@ angular.module('controllers', ['luegg.directives'])
             });
 
             $scope.alreadyJoinPatientConversationContent = [
-                {
+                {}
+                /*
                     "patientId":"aaaa",
                     "patientName": "frank",
                 },
                 {
                     "patientId":"wefedfwe",
                     "patientName": "frank",
-                }
+                }*/
             ];
 
             getCommonAnswerList.save({"type": "commonAnswer"}, function (data) {
@@ -522,6 +525,7 @@ angular.module('controllers', ['luegg.directives'])
                     console.log(data);
                     $scope.defaultClickUserOpenId = openid;
                     $scope.currentUserConsultRecordDetail = data.records;
+
                 });
             }
 
@@ -559,13 +563,13 @@ angular.module('controllers', ['luegg.directives'])
                     }, function (data) {
                         $scope.userConsultListInfo = data.userList;
                         $scope.currentUserConsultRecordDetail = data.records;
-                        if (senderId==doctor){
-
-                        }
                     });
                 }
             }
-
+/*            if (currentUserConsultRecordDetail.senderId == doctor){
+                $(this).css('color','#7fc700');
+                console.log($(this))
+            }*/
             //查找咨询记录
             $scope.setSearchMessageType = function (searchType) {
                 $scope.messageType = searchType;
