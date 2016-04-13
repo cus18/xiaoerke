7120202d167180ad6d39929c4d2113fbdd34b243
zhouguangyue <zhouGUANGYUE666>
2016-04-13 18:17:09 +0800

    update

---
+++ doctor/src/main/webapp/js/controllers/phoneConsult/phoneConsultFirstCtrl.js
@@ -0,0 +1,22 @@
+angular.module('controllers', ['ionic']).controller('phoneConsultFirstCtrl', [
+    '$scope','$ionicPopup','$state','$stateParams','$location','GetUserLoginStatus',
+    function ($scope,$ionicPopup,$state,$stateParams,$location,GetUserLoginStatus) {
+        $scope.title = "电话咨询";
+
+
+        $scope.$on('$ionicView.enter', function(){
+            $scope.pageLoading = true;
+            var routePath = "/phoneConsultDoctorBBBBBB" + $location.path();
+            GetUserLoginStatus.save({routePath:routePath},function(data){
+                $scope.pageLoading = false;
+                if(data.status=="9") {
+                    window.location.href = data.redirectURL;
+                }else if(data.status=="8"){
+                    window.location.href = data.redirectURL+"?targeturl="+routePath;
+                }else{
+
+                }
+            })
+        });
+    }]);
+
