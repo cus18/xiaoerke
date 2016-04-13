42317f5be52021080a725327c4ed37caa7631f6f
izhengly <820359064@qq.com>
2016-04-13 11:28:29 +0800

    phoneConsult update

--- keeper/src/main/webapp/js/phoneConsultPay.js
+++ keeper/src/main/webapp/js/phoneConsultPay.js
@@ -199,6 +199,14 @@ function loadDate(){
     };
     $("#birthday").mobiscroll(opt);
 }
+// 点击跳转到首页
+var goFirstPage=function(){
+    window.location.href="/titan/firstPage/phoneConsult"
+}
+// 点击跳转到个人中心
+var goMyselfCenter=function(){
+    window.location.href="/titan/phoneConsult#/selfCenter"
+}
 // 点击选择宝宝按钮
 var selectBaby=function(){
     $(".baby-list").show();
