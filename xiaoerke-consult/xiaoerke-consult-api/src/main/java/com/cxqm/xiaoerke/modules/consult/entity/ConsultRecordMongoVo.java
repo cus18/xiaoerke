d8c45fbbbe7ec1a44cf72c6cae56fc8a24b113b9
cus18 <cus18@sina.com>
2016-04-18 14:33:34 +0800

    123

--- xiaoerke-consult/xiaoerke-consult-api/src/main/java/com/cxqm/xiaoerke/modules/consult/entity/ConsultRecordMongoVo.java
+++ xiaoerke-consult/xiaoerke-consult-api/src/main/java/com/cxqm/xiaoerke/modules/consult/entity/ConsultRecordMongoVo.java
@@ -29,9 +29,9 @@ public class ConsultRecordMongoVo {
 
     private String opercode;
 
-    private String fromUserId;
+    private String userId;
 
-    private String toUserId;
+    private String csUserId;
 
     private String senderId;
 
@@ -189,20 +189,20 @@ public class ConsultRecordMongoVo {
         this.opercode = opercode;
     }
 
-    public String getFromUserId() {
-        return fromUserId;
+    public String getUserId() {
+        return userId;
     }
 
-    public void setFromUserId(String fromUserId) {
-        this.fromUserId = fromUserId;
+    public void setUserId(String userId) {
+        this.userId = userId;
     }
 
-    public String getToUserId() {
-        return toUserId;
+    public String getCsUserId() {
+        return csUserId;
     }
 
-    public void setToUserId(String toUserId) {
-        this.toUserId = toUserId;
+    public void setCsUserId(String csUserId) {
+        this.csUserId = csUserId;
     }
 
     public String getConsultType() {
