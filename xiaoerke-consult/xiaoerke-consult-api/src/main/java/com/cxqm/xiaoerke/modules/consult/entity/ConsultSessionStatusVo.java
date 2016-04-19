c06fa3d120c876870b84fab6f51c42bef3551202
cus18 <cus18@sina.com>
2016-04-19 15:32:27 +0800

    123

--- xiaoerke-consult/xiaoerke-consult-api/src/main/java/com/cxqm/xiaoerke/modules/consult/entity/ConsultSessionStatusVo.java
+++ xiaoerke-consult/xiaoerke-consult-api/src/main/java/com/cxqm/xiaoerke/modules/consult/entity/ConsultSessionStatusVo.java
@@ -11,6 +11,16 @@ public class ConsultSessionStatusVo {
 
     private String UserId;
 
+    private String openId;
+
+    public String getOpenId() {
+        return openId;
+    }
+
+    public void setOpenId(String openId) {
+        this.openId = openId;
+    }
+
     private ConsultSessionStatusVo consultSessionStatusVo;
 
     public String getUserId() {
