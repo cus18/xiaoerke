c79e7b6c6ae2c281816fad56adf274d12ea77b8a
cus18 <cus18@sina.com>
2016-04-21 14:55:04 +0800

    123

--- xiaoerke-consult/xiaoerke-consult-api/src/main/java/com/cxqm/xiaoerke/modules/consult/service/ConsultMongoUtilsService.java
+++ xiaoerke-consult/xiaoerke-consult-api/src/main/java/com/cxqm/xiaoerke/modules/consult/service/ConsultMongoUtilsService.java
@@ -2,6 +2,8 @@ package com.cxqm.xiaoerke.modules.consult.service;
 
 import com.cxqm.xiaoerke.modules.consult.entity.ConsultRecordMongoVo;
 import com.cxqm.xiaoerke.modules.consult.entity.RichConsultSession;
+import com.mongodb.AggregationOutput;
+import com.mongodb.DBObject;
 import com.mongodb.WriteResult;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.Update;
@@ -20,7 +22,7 @@ public interface  ConsultMongoUtilsService {
 
 	WriteResult removeConsultRankRecord(Query query);
 
-	List<ConsultRecordMongoVo> queryConsultRankRecordDistinct(String key,Query query);
+	AggregationOutput queryConsultRankRecordGroup(DBObject group);
 
 	List<String> queryConsultRankUserCount(String key,Query query);
 }
