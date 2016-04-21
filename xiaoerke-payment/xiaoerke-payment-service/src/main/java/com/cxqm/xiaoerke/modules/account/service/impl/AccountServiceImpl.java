5a96ab8baac9d225343825241167c84f48db9597
张博 <zbm0915@gmail.com>
2016-04-21 11:45:01 +0800

    测试新版支付

--- xiaoerke-payment/xiaoerke-payment-service/src/main/java/com/cxqm/xiaoerke/modules/account/service/impl/AccountServiceImpl.java
+++ xiaoerke-payment/xiaoerke-payment-service/src/main/java/com/cxqm/xiaoerke/modules/account/service/impl/AccountServiceImpl.java
@@ -385,8 +385,8 @@ public class AccountServiceImpl implements AccountService {
         }
 
         //获取需要支付的金额  单位(分)
-        int price = ((Float)request.getAttribute("payPrice")).intValue()*100;
-        String order_price =StringUtils.isNotNull(String.valueOf(price))?String.valueOf(price):request.getParameter("payPrice");
+//        int price = ((Float)request.getAttribute("payPrice")).intValue()*100;
+        String order_price =StringUtils.isNotNull(String.valueOf((request.getAttribute("payPrice"))))?String.valueOf(((Float)request.getAttribute("payPrice")).intValue()*100):request.getParameter("payPrice");
         order_price = "1";
         //生成的商户订单号
         String out_trade_no = IdGen.uuid();//Sha1Util.getNonceStr();
