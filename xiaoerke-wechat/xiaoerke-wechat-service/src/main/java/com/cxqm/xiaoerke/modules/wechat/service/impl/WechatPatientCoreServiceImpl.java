ded0b241db6f66e8ab8309478139c1e2ed0110d8
cjk18 <cus18@sina.com>
2016-04-27 18:52:22 +0800

    new merge

--- xiaoerke-wechat/xiaoerke-wechat-service/src/main/java/com/cxqm/xiaoerke/modules/wechat/service/impl/WechatPatientCoreServiceImpl.java
+++ xiaoerke-wechat/xiaoerke-wechat-service/src/main/java/com/cxqm/xiaoerke/modules/wechat/service/impl/WechatPatientCoreServiceImpl.java
@@ -143,7 +143,7 @@ public class WechatPatientCoreServiceImpl implements WechatPatientCoreService {
 			try {
 				System.out.println(xmlEntity.getContent());
 				if(xmlEntity.getMsgType().equals("text")){
-					this.sendPost("http://192.168.191.2:8080/angel/consult/wechat/conversation",
+					this.sendPost("http://xiaork.com/angel/consult/wechat/conversation",
 							"openId=" + xmlEntity.getFromUserName() +
 							"&messageType=" + xmlEntity.getMsgType() +
 							"&messageContent=" + URLEncoder.encode(xmlEntity.getContent(), "UTF-8"));
