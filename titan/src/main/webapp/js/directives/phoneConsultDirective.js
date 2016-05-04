f22feebe73e0d26f779665d8a4cd404a526c630e
izhengly <820359064@qq.com>
2016-05-04 19:16:19 +0800

    常见问题 链接修改

--- titan/src/main/webapp/js/directives/phoneConsultDirective.js
+++ titan/src/main/webapp/js/directives/phoneConsultDirective.js
@@ -37,11 +37,14 @@ define(['appPhoneConsult','jquery'], function (app,$) {
                 replace: true,
                 template: '<div class="footer tc">' +
                 '<p><a class="c1 f2 home" ng-click="appointmentFirst()">首页</a>' +
-                '<a class="c1 f2" ui-sref="questions">常见问题</a></p>' +
+                '<a class="c1 f2" ng-click="commonQuestion()">常见问题</a></p>' +
                 '</p><a class="c1 f2" href="tel:4006237120">客服电话：400-623-7120</a></p></div>',
                 link: function(scope) {
                     scope.appointmentFirst = function(){
                         window.location.href = "firstPage/phoneConsult";
+                    }
+                    scope.commonQuestion = function(){
+                        window.location.href = "baoFansCamp#/questions";
                     }
                 }
             }
