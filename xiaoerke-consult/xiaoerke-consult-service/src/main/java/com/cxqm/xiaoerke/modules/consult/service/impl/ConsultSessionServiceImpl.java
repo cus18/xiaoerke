420d4f1ba0ecb5db4233617e0652fb0d91eb27cd
Administrator <565871712@qq.com>
2016-05-05 09:25:32 +0800

    update

--- xiaoerke-consult/xiaoerke-consult-service/src/main/java/com/cxqm/xiaoerke/modules/consult/service/impl/ConsultSessionServiceImpl.java
+++ xiaoerke-consult/xiaoerke-consult-service/src/main/java/com/cxqm/xiaoerke/modules/consult/service/impl/ConsultSessionServiceImpl.java
@@ -83,21 +83,20 @@ public class ConsultSessionServiceImpl implements ConsultSessionService {
                 consultSession.setStatus(ConsultSession.STATUS_COMPLETED);
             }
 
-            int status = this.updateSessionInfo(consultSession);
-            if(status==1){
-                //清除redis内的数据
-                sessionRedisCache.removeConsultSessionBySessionId(Integer.parseInt(sessionId));
-                sessionRedisCache.removeUserIdSessionIdPair(userId);
+            this.updateSessionInfo(consultSession);
 
-                //清除内存内的数据
-                ConsultSessionManager.getSessionManager().removeUserSession(userId);
+            //清除redis内的数据
+            sessionRedisCache.removeConsultSessionBySessionId(Integer.parseInt(sessionId));
+            sessionRedisCache.removeUserIdSessionIdPair(userId);
 
-                //更新最后一次会话
-                consultRecordService.updateConsultSessionStatusVo(new Query().addCriteria(new Criteria().where("sessionId").is(sessionId)),"complete");
+            //清除内存内的数据
+            ConsultSessionManager.getSessionManager().removeUserSession(userId);
 
-                //删除用户的临时聊天记录
-                consultRecordService.deleteConsultTempRecordVo(new Query().addCriteria(new Criteria().where("userId").is(userId)));
-            }
+            //更新最后一次会话
+            consultRecordService.updateConsultSessionStatusVo(new Query().addCriteria(new Criteria().where("sessionId").is(sessionId)),"complete");
+
+            //删除用户的临时聊天记录
+            consultRecordService.deleteConsultTempRecordVo(new Query().addCriteria(new Criteria().where("userId").is(userId)));
 
             return "success";
         }catch (Exception e){
