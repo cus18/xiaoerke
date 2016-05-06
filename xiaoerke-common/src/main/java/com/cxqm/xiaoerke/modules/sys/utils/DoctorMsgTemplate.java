4ae7eaf97b8d0d15485396d758934ce0500ce5a5
chenxiaoqiong <635264100@qq.com>
2016-05-06 16:42:49 +0800

    短信中url>shortUrl

--- xiaoerke-common/src/main/java/com/cxqm/xiaoerke/modules/sys/utils/DoctorMsgTemplate.java
+++ xiaoerke-common/src/main/java/com/cxqm/xiaoerke/modules/sys/utils/DoctorMsgTemplate.java
@@ -2,6 +2,7 @@ package com.cxqm.xiaoerke.modules.sys.utils;
 
 import com.cxqm.xiaoerke.common.bean.WechatArticle;
 import com.cxqm.xiaoerke.common.utils.StringUtils;
+import com.cxqm.xiaoerke.common.utils.WechatUtil;
 
 import java.util.ArrayList;
 
@@ -157,8 +158,8 @@ public class DoctorMsgTemplate {
     }
 
 
-    public static void doctorPhoneConsultRemindAt5minBefore2Sms(String doctorName,String babyName,String phone,String userPhone,String url){
-        String content = "【接听提醒】尊敬的"+doctorName+"医生，"+babyName+"小朋友家长将在5min以后接通电话咨询，到时您会接到号码为"+userPhone+"的来电，请保持电话畅通。在这之前，您可以打开链接，查看患者详细的病情资料"+url;
+    public static void doctorPhoneConsultRemindAt5minBefore2Sms(String token,String doctorName,String babyName,String phone,String userPhone,String url){
+        String content = "【接听提醒】尊敬的"+doctorName+"医生，"+babyName+"小朋友家长将在5min以后接通电话咨询，到时您会接到号码为"+userPhone+"的来电，请保持电话畅通。在这之前，您可以打开链接，查看患者详细的病情资料"+ WechatUtil.getShortUrl(token,url);
         SMSMessageUtil.sendMsg(phone, content);
     }
 
