0d2f732e4ee9706479e171d09f97225d4e749397
王保伟 <565871712@qq.com>
2016-05-09 14:19:26 +0800

    打入日志点

--- xiaoerke-common/src/main/java/com/cxqm/xiaoerke/common/utils/ConstantUtil.java
+++ xiaoerke-common/src/main/java/com/cxqm/xiaoerke/common/utils/ConstantUtil.java
@@ -8,35 +8,39 @@ public class ConstantUtil {
 	public static final String AMR_TOMP3_LINUXPATH = "/mnt/temp";
 
 	//初始化
-//	public static String APP_ID = "wx0baf90e904df0117";//微信开发平台应用id
-//	public static String PARTNER = "1260344901";//财付通商户号
-//	public static String PARTNER_KEY = "chenxingqiming00chenxingqiming00";//商户号对应的密钥
-//	public static String GATEURL = "https://api.mch.weixin.qq.com/pay/unifiedorder";//获取预支付id的接口url
-//	public static String TRANSFERS = "https://api.mch.weixin.qq.com/mmpaymkttransfers/promotion/transfers";//获取预支付id的接口url
-//	public static String PAYRESULT = "https://api.mch.weixin.qq.com/pay/orderquery";//获取支付结果
-//	public static String SIGN_METHOD = "MD5";//签名算法常量值
-
-	//初始化
-	public static String APP_ID = "wxa19496b1076e7352";//微信开发平台应用id
-	public static String PARTNER = "1252031301";//财付通商户号
+	public static String APP_ID = "wx0baf90e904df0117";//微信开发平台应用id
+	public static String PARTNER = "1260344901";//财付通商户号
 	public static String PARTNER_KEY = "chenxingqiming00chenxingqiming00";//商户号对应的密钥
 	public static String GATEURL = "https://api.mch.weixin.qq.com/pay/unifiedorder";//获取预支付id的接口url
 	public static String TRANSFERS = "https://api.mch.weixin.qq.com/mmpaymkttransfers/promotion/transfers";//获取预支付id的接口url
 	public static String PAYRESULT = "https://api.mch.weixin.qq.com/pay/orderquery";//获取支付结果
 	public static String SIGN_METHOD = "MD5";//签名算法常量值
 
-	public static String NOTIFY_APPOINT_URL = "http://xiaork.cn/titan/user/getPayNotifyInfo";
-	public static String NOTIFY_INSURANCE_URL = "http://xiaork.cn/titan/user/getInsurancePayNotifyInfo";
-	public static String NOTIFY_CUSTOMER_URL = "http://xiaork.cn/titan/user/getCustomerPayNotifyInfo";
-	public static String NOTIFY_CONSULTPHONE_URL = "http://xiaork.cn/titan/user/getPhoneConsultPayNotifyInfo";
+	//初始化
+//	public static String APP_ID = "wxa19496b1076e7352";//微信开发平台应用id
+//	public static String PARTNER = "1252031301";//财付通商户号
+//	public static String PARTNER_KEY = "chenxingqiming00chenxingqiming00";//商户号对应的密钥
+//	public static String GATEURL = "https://api.mch.weixin.qq.com/pay/unifiedorder";//获取预支付id的接口url
+//	public static String TRANSFERS = "https://api.mch.weixin.qq.com/mmpaymkttransfers/promotion/transfers";//获取预支付id的接口url
+//	public static String PAYRESULT = "https://api.mch.weixin.qq.com/pay/orderquery";//获取支付结果
+//	public static String SIGN_METHOD = "MD5";//签名算法常量值
+//
+	public static String NOTIFY_APPOINT_URL = "http://s68.baodf.com:8081/titan/user/getPayNotifyInfo";
+	public static String NOTIFY_INSURANCE_URL = "http://s68.baodf.com:8081/titan/user/getInsurancePayNotifyInfo";
+	public static String NOTIFY_CUSTOMER_URL = "http://s68.baodf.com:8081/titan/user/getCustomerPayNotifyInfo";
+	public static String NOTIFY_CONSULTPHONE_URL = "hhttp://s68.baodf.com:8081/titan/user/getPhoneConsultPayNotifyInfo";
 
 	public static int DAY_QUARTER_NUMBER = 96;
 	public static int VISIT_INTERVAL = 15;
 	public static String MARKET_WEB_URL = "http://xiaork.com/";
-	public static String WISDOM_WEB_URL = "http://xiaork.com/";
-	public static String ANGEL_WEB_URL = "http://xiaork.com/";
-	public static String KEEPER_WEB_URL = "http://xiaork.cn/";
-	public static String DOCTOR_WEB_URL = "http://xiaork.cn/";
-	public static String TITAN_WEB_URL = "http://xiaork.cn/";
+	public static String WISDOM_WEB_URL = "http://s165.baodf.com:8081/";
+	public static String ANGEL_WEB_URL = "http://s34.baodf.com:8081/";
+	public static String KEEPER_WEB_URL = "http://s251.baodf.com:8081/";
+	public static String DOCTOR_WEB_URL = "http://s68.baodf.com:8081/";
+	public static String TITAN_WEB_URL = "http://s68.baodf.com:8081/";
+
+
+
+
 
 }
