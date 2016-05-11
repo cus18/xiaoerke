470c27cfce4853ae81d5c72005609bf04189c6e7
张博 <zbm0915@gmail.com>
2016-05-11 14:45:02 +0800

    运维统计修改及献爱心运营活动

---
+++ xiaoerke-web/src/main/java/com/cxqm/xiaoerke/modules/marketing/web/LoveMarketingController.java
@@ -0,0 +1,37 @@
+package com.cxqm.xiaoerke.modules.marketing.web;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.cxqm.xiaoerke.common.utils.WechatUtil;
+
+
+@Controller
+@RequestMapping(value = "loveMarketing")
+public class LoveMarketingController {
+
+
+
+
+
+
+
+
+}
