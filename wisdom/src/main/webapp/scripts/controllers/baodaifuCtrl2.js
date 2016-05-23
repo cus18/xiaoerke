7f60841fc688442814935123fd3705d0f36972aa
zhouguangyue <zhouGUANGYUE666>
2016-05-23 16:00:35 +0800

    update

--- wisdom/src/main/webapp/scripts/controllers/baodaifuCtrl2.js
+++ wisdom/src/main/webapp/scripts/controllers/baodaifuCtrl2.js
@@ -3,18 +3,43 @@ angular.module('controllers2', [])
         function ($scope,$state,$stateParams) {
 
             $(function(){
+                $(".img_weixin").hide();
                 $('#mov_doctor').movingBoxes({
                     width: 1000,
                     reducedSize : 0.5,
                     startPanel : 2,
                     currentPanel : 'svccurrent',
+                    fixedHeight:false,
                     hashTags: false,
                     wrap: false,
+                    initialized: function(e, slider, tar){
+                        //console.log("1",e);
+                        //console.log("2",slider);
+                        //console.log("3",tar);
+                        slider.curWidth = 300;
+                        slider.regWidth = 150;
+                        slider.$curPanel.find('.mb-scroll').css("height","315px");
+                        slider.$curPanel.find('.doc_name').hide();
+                        slider.$curPanel.find('.doc_name_info').css("font-size","20px");
+                        slider.$curPanel.find('.doc_hosp').css("font-size","18px");
+                },
                     initChange: function(e, slider, tar){
+                        slider.curWidth = 300;
+                        slider.regWidth = 150;
+                        slider.$curPanel.find('.doc_name').show();
+                        slider.$curPanel.find('.doc_name_info').css("font-size","0px");
+                        slider.$curPanel.find('.doc_hosp').css("font-size","0px");
+                    },
+                    completed: function(e, slider){
                         console.log("1",e);
                         console.log("2",slider);
-                        console.log("3",tar);
                         slider.curWidth = 300;
+                        slider.regWidth = 150;
+                        slider.$curPanel.clientHeight = 315;
+                        slider.$curPanel.find('.mb-scroll').css("height","315px");
+                        slider.$curPanel.find('.doc_name').hide();
+                        slider.$curPanel.find('.doc_name_info').css("font-size","20px");
+                        slider.$curPanel.find('.doc_hosp').css("font-size","18px");
                     }
 
                 });
@@ -24,16 +49,38 @@ angular.module('controllers2', [])
                     startPanel : 2,
                     currentPanel : 'svccurrent',
                     hashTags: false,
+                    fixedHeight:false,
                     wrap: false,
-                    initChange: function(e, slider, tar){
-                        console.log("1",e);
-                        console.log("2",slider);
-                        console.log("3",tar);
+                    initialized: function(e, slider, tar){
+                        //console.log("1",e);
+                        //console.log("2",slider);
+                        //console.log("3",tar);
                         slider.curWidth = 300;
+                        slider.regWidth = 150;
+                        slider.$curPanel.find('.mb-scroll').css("height","315px");
+                        slider.$curPanel.find('.doc_name').css("margin-top","0px");
+                    },
+                    initChange: function(e, slider, tar){
+                        slider.curWidth = 300;
+                        slider.regWidth = 150;
+                    },
+                    completed: function(e, slider){
+                        slider.curWidth = 300;
+                        slider.regWidth = 150;
+                        slider.$curPanel.find('.mb-scroll').css("height","315px");
+                        slider.$curPanel.find('.doc_name').css("margin-top","0px");
                     }
 
                 });
             })
 
+            $(".img_1").mouseenter(function () {
+                $(".img_weixin").show();
+
+            });
+            $(".img_1").mouseout(function () {
+                $(".img_weixin").hide();
+
+            });
 
         }]);
