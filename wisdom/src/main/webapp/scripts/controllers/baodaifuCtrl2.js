99c384d972ba6319098e5090f02ec10ffbe533dc
zhouguangyue <zhouGUANGYUE666>
2016-05-23 18:34:58 +0800

    update

--- wisdom/src/main/webapp/scripts/controllers/baodaifuCtrl2.js
+++ wisdom/src/main/webapp/scripts/controllers/baodaifuCtrl2.js
@@ -16,16 +16,16 @@ angular.module('controllers2', [])
                         //console.log("1",e);
                         //console.log("2",slider);
                         //console.log("3",tar);
-                        slider.curWidth = 300;
-                        slider.regWidth = 150;
-                        slider.$curPanel.find('.mb-scroll').css("height","315px");
+                        slider.curWidth = 225;
+                        slider.regWidth = 110;
                         slider.$curPanel.find('.doc_name').hide();
                         slider.$curPanel.find('.doc_name_info').css("font-size","20px");
                         slider.$curPanel.find('.doc_hosp').css("font-size","18px");
+
                 },
                     initChange: function(e, slider, tar){
-                        slider.curWidth = 300;
-                        slider.regWidth = 150;
+                        slider.curWidth = 225;
+                        slider.regWidth = 110;
                         slider.$curPanel.find('.doc_name').show();
                         slider.$curPanel.find('.doc_name_info').css("font-size","0px");
                         slider.$curPanel.find('.doc_hosp').css("font-size","0px");
@@ -33,10 +33,8 @@ angular.module('controllers2', [])
                     completed: function(e, slider){
                         console.log("1",e);
                         console.log("2",slider);
-                        slider.curWidth = 300;
-                        slider.regWidth = 150;
-                        slider.$curPanel.clientHeight = 315;
-                        slider.$curPanel.find('.mb-scroll').css("height","315px");
+                        slider.curWidth = 225;
+                        slider.regWidth = 110;
                         slider.$curPanel.find('.doc_name').hide();
                         slider.$curPanel.find('.doc_name_info').css("font-size","20px");
                         slider.$curPanel.find('.doc_hosp').css("font-size","18px");
@@ -55,19 +53,17 @@ angular.module('controllers2', [])
                         //console.log("1",e);
                         //console.log("2",slider);
                         //console.log("3",tar);
-                        slider.curWidth = 300;
-                        slider.regWidth = 150;
-                        slider.$curPanel.find('.mb-scroll').css("height","315px");
+                        slider.curWidth = 250;
+                        slider.regWidth = 125;
                         slider.$curPanel.find('.doc_name').css("margin-top","0px");
                     },
                     initChange: function(e, slider, tar){
-                        slider.curWidth = 300;
-                        slider.regWidth = 150;
+                        slider.curWidth = 250;
+                        slider.regWidth = 125;
                     },
                     completed: function(e, slider){
-                        slider.curWidth = 300;
-                        slider.regWidth = 150;
-                        slider.$curPanel.find('.mb-scroll').css("height","315px");
+                        slider.curWidth = 250;
+                        slider.regWidth = 125;
                         slider.$curPanel.find('.doc_name').css("margin-top","0px");
                     }
 
@@ -97,7 +93,36 @@ angular.module('controllers2', [])
 
             $(function () {
                 $(".img_weixin").hide();
+
+                //顶部菜单效果
+                var $headBar = $('.index_title'), initTop = 0, isScroll = true;
+                $(window).on('scroll', function(e) {
+                    var scrollY = $(this).scrollTop();
+                    if(scrollY > 126){
+                        if(scrollY > initTop){
+                            $headBar.addClass('active');
+                        }else{
+                            $headBar.removeClass('active');
+                        }
+                    } else{
+                        $headBar.removeClass('active');
+                    }
+                    initTop = scrollY;
+                    //if(scrollY >= (3000 - $(window).height())){
+                    //    $('.sy-connect .left').animate({'margin-left': '0'}, '1000');
+                    //    $('.sy-connect .right').animate({'margin-right': '0'}, '1000');
+                    //}
+                });
+
+
+
             });
+
+            //首页
+            $scope.goIndex = function () {
+                 $state.go("index");
+            }
+
 
             //底部微信二维码显示
             $(".img_1").mouseenter(function () {
