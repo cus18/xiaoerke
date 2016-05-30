315d2c8318a6abdaecc89a9de6453c7e56bfa39b
cjk18 <cus18@sina.com>
2016-05-30 13:06:31 +0800

    new merge

--- xiaoerke-web/src/main/java/com/cxqm/xiaoerke/modules/consult/web/ConsultWechatController.java
+++ xiaoerke-web/src/main/java/com/cxqm/xiaoerke/modules/consult/web/ConsultWechatController.java
@@ -145,20 +145,17 @@ public class ConsultWechatController extends BaseController {
             if(sessionId!=null){
                 consultSession = sessionRedisCache.getConsultSessionBySessionId(sessionId);
                 csChannel = ConsultSessionManager.getSessionManager().getUserChannelMapping().get(consultSession.getCsUserId());
-                if(csChannel == null){
-                    consultSessionService.clearSession(String.valueOf(sessionId),userId);
-                    //重新未用户分配到接诊员，开启新的会话
-                    consultSession.setCreateTime(new Date());
-                    consultSession.setUserId(userId);
-                    consultSession.setUserName(userName);
-                    consultSession.setSource(source);
-                    consultSession.setServerAddress(serverAddress);
-                    //创建会话，发送消息给用户，给用户分配接诊员
-                    createWechatConsultSessionMap = ConsultSessionManager.getSessionManager().createUserWXConsultSession(consultSession);
-                    if(createWechatConsultSessionMap!=null){
-                        csChannel = (Channel)createWechatConsultSessionMap.get("csChannel");
-                        consultSession = (RichConsultSession)createWechatConsultSessionMap.get("consultSession");
-                        sessionId = consultSession.getId();
+                if(csChannel==null){
+                    //保存聊天记录
+                    consultRecordService.buildRecordMongoVo(userId, String.valueOf(ConsultUtil.transformMessageTypeToType(messageType)), messageContent, consultSession);
+                    //更新会话操作时间
+                    consultRecordService.saveConsultSessionStatus(consultSession);
+                }else{
+                    if(!csChannel.isActive()){
+                        //保存聊天记录
+                        consultRecordService.buildRecordMongoVo(userId, String.valueOf(ConsultUtil.transformMessageTypeToType(messageType)), messageContent, consultSession);
+                        //更新会话操作时间
+                        consultRecordService.saveConsultSessionStatus(consultSession);
                     }
                 }
             }else{
@@ -187,6 +184,7 @@ public class ConsultWechatController extends BaseController {
                     obj.put("senderName",userName);
                     obj.put("serverAddress",serverAddress);
                     obj.put("source",consultSession.getSource());
+
 
                     StringBuffer sbf = new StringBuffer();
                     if(messageType.equals("text")) {
@@ -228,8 +226,7 @@ public class ConsultWechatController extends BaseController {
                     csChannel.writeAndFlush(frame.retain());
 
                     //保存聊天记录
-                    consultRecordService.buildRecordMongoVo(userId,String.valueOf(ConsultUtil.transformMessageTypeToType(messageType)),
-                            messageContent, consultSession);
+                    consultRecordService.buildRecordMongoVo(userId,String.valueOf(ConsultUtil.transformMessageTypeToType(messageType)), messageContent, consultSession);
 
                     //更新会话操作时间
                     consultRecordService.saveConsultSessionStatus(consultSession);
