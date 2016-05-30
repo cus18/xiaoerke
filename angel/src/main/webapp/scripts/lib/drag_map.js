22e1abd8cc39b170581a8cef5b6883b74049d9d5
lipangpangz <270255061>
2016-05-30 15:50:38 +0800

    big

--- angel/src/main/webapp/scripts/lib/drag_map.js
+++ angel/src/main/webapp/scripts/lib/drag_map.js
@@ -108,3 +108,46 @@ if(tZoom<0.1 ) return true;
 obj.style.zoom=tZoom;
 return false;
 }
+
+var x, y, n = 0,
+    ny = 0,
+    rotINT, rotYINT;
+function rotateDIV() {
+ x = document.getElementById("rotate1");
+ clearInterval(rotINT);
+ rotINT = setInterval("startRotate()", 0)
+}
+
+function rotateYDIV() {
+ y = document.getElementById("rotatey1");
+ clearInterval(rotYINT);
+ rotYINT = setInterval("startYRotate()", 0)
+}
+
+function startRotate() {
+ n = n + 1;
+ x.style.transform = "rotate(" + n + "deg)";
+ x.style.webkitTransform = "rotate(" + n + "deg)";
+ x.style.OTransform = "rotate(" + n + "deg)";
+ x.style.MozTransform = "rotate(" + n + "deg)";
+ if (n == 90 || n == 180 || n == 270 || n == 360) {
+  clearInterval(rotINT);
+  if (n == 360) {
+   n = 0
+  }
+ }
+}
+
+function startYRotate() {
+ ny = ny + 1;
+ y.style.transform = "rotateY(" + ny + "deg)";
+ y.style.webkitTransform = "rotateY(" + ny + "deg)";
+ y.style.OTransform = "rotateY(" + ny + "deg)";
+ y.style.MozTransform = "rotateY(" + ny + "deg)";
+ if (ny == 90 || ny >= 180 || ny == 270 || ny >= 360) {
+  clearInterval(rotYINT);
+  if (ny >= 360) {
+   ny = 0
+  }
+ }
+}
