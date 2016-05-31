a30896e9bbdedc96acbf0b33d72cf03a9d420c9c
张博 <zbm0915@gmail.com>
2016-05-31 19:21:53 +0800

    保护伞

--- xiaoerke-web/src/main/java/com/cxqm/xiaoerke/modules/umbrella/web/UmbrellaController.java
+++ xiaoerke-web/src/main/java/com/cxqm/xiaoerke/modules/umbrella/web/UmbrellaController.java
@@ -181,7 +181,7 @@ public class UmbrellaController  {
         if(openIdStatus != null){
             String status=openIdStatus.get("status").toString();
             if(status.equals("0")) {
-                result.put("status", "1");
+                result.put("status", "0");
                 result.put("openid", openid);
                 return result;
             }else{
