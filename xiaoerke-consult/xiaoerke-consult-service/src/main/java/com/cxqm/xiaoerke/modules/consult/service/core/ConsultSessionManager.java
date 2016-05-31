fc079a45c679edbd1c4899afde186c85ad56a338
Administrator <565871712@qq.com>
2016-05-31 13:33:56 +0800

    new merge

--- xiaoerke-consult/xiaoerke-consult-service/src/main/java/com/cxqm/xiaoerke/modules/consult/service/core/ConsultSessionManager.java
+++ xiaoerke-consult/xiaoerke-consult-service/src/main/java/com/cxqm/xiaoerke/modules/consult/service/core/ConsultSessionManager.java
@@ -632,7 +632,7 @@ public class ConsultSessionManager {
 			consultSession.setId(richConsultSession.getId());
 			consultSession.setCsUserId(richConsultSession.getCsUserId());
 			consultSession.setStatus("ongoing");
-			consultSession.setCreateTime(richConsultSession.getCreateTime());
+			consultSession.setCreateTime(new Date());
 			flag = consultSessionService.updateSessionInfo(consultSession);
 		}else{
 			consultSession.setCsUserId(richConsultSession.getCsUserId());
