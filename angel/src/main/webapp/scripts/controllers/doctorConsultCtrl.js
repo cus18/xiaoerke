d9469b78104780d9b1d59f1f793ba5e203e80f1e
Administrator <565871712@qq.com>
2016-05-31 14:57:04 +0800

    new merge

--- angel/src/main/webapp/scripts/controllers/doctorConsultCtrl.js
+++ angel/src/main/webapp/scripts/controllers/doctorConsultCtrl.js
@@ -829,8 +829,10 @@ angular.module('controllers', ['luegg.directives'])
 
             //查看更多的用户历史消息
             $scope.seeMoreConversationMessage = function(){
-                var mostFarCurrentConversationDateTime = $scope.currentUserConversation.consultValue[0].dateTime;
-                console.log("mostFarCurrentConversationDateTime",mostFarCurrentConversationDateTime);
+                var mostFarCurrentConversationDateTime = moment().format("YYYY-MM-DD HH:mm:ss");
+                if($scope.currentUserConversation.consultValue[0]!=undefined){
+                    var mostFarCurrentConversationDateTime = $scope.currentUserConversation.consultValue[0].dateTime;
+                }
                 GetCurrentUserHistoryRecord.save({
                     userId:$scope.currentUserConversation.patientId,
                     dateTime:mostFarCurrentConversationDateTime,
