ecd9fdeeccd959316b063bc1c52013f5e14afd36
张博 <zbm0915@gmail.com>
2016-06-03 18:19:28 +0800

    保护伞

--- keeper/src/main/webapp/js/pay/umbrellaPay.js
+++ keeper/src/main/webapp/js/pay/umbrellaPay.js
@@ -98,14 +98,15 @@ var doRefresh = function(){
                     // config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，
                     // 则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，
                     // 则可以直接调用，不需要放在ready函数中。
-                })
+                });
+                wx.hideOptionMenu();
             }else{
             }
         },
         error : function() {
         }
     });
-    wx.hideOptionMenu();
+
 }
 
 function wechatPay() {
