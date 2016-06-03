f5fcb385fe22ecc6336af0278c918a4cc11bd3d7
izhengly <820359064@qq.com>
2016-06-03 17:44:16 +0800

    宝护伞家庭版新添加页面

---
+++ wisdom/src/main/webapp/js/controllers/umbrella/umbrellaMemberListCtrl.js
@@ -0,0 +1,19 @@
+﻿angular.module('controllers', ['ionic']).controller('umbrellaMemberListCtrl', [
+        '$scope','$state','$stateParams',
+        function ($scope,$state,$stateParams) {
+            $scope.title="宝护伞";
+
+
+            $scope.goActive=function(){
+                $state.go("umbrellaFillInfo",{id:$scope.umbrellaId});
+            };
+            $scope.goShare=function(){
+                $scope.shareLock=true;
+            };
+
+            $scope.$on('$ionicView.enter', function(){
+
+            });
+
+            
+    }]);
