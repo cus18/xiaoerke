501830210ca7c523f7559a33854a7ac10a86ddd9
王保伟 <565871712@qq.com>
2016-06-06 15:35:20 +0800

    增加dao

--- wisdom/src/main/webapp/js/controllers/umbrella/umbrellaMemberListCtrl.js
+++ wisdom/src/main/webapp/js/controllers/umbrella/umbrellaMemberListCtrl.js
@@ -4,8 +4,29 @@
             $scope.title="宝护伞";
             //$scope.id = $stateParams.id;
 
+
             getFamilyList.save({"id":$stateParams.id},function(data){
                 console.log(data);
+                $scope.familyList =data.familyList;
+                for(var i=0;i<data.familyList.length;i++){
+                    $scope.familyList[i].birthday = moment(data.familyList[i].birthday).format("YYYY-MM-DD");
+                    var birth = moment(data.familyList[i].birthday);
+                    var nowTime = moment();
+                    var age = nowTime.diff(birth, 'years');       // 1
+                    if(age>18){
+                      if($scope.familyList[i].sex==0){
+                          $scope.familyList[i].sex = "宝爸"
+                      }else{
+                          $scope.familyList[i].sex = "宝妈"
+                      }
+                    }else{
+                        if($scope.familyList[i].sex==0){
+                            $scope.familyList[i].sex = "男宝"
+                        }else{
+                            $scope.familyList[i].sex = "女宝"
+                        }
+                    }
+                }
             })
 
             $scope.addMember=function(){
