91dc6f5149b53459085629bb9851dac84543657a
王保伟 <565871712@qq.com>
2016-06-08 00:48:18 +0800

    时间

--- wisdom/src/main/webapp/js/controllers/umbrella/umbrellaJoinCtrl.js
+++ wisdom/src/main/webapp/js/controllers/umbrella/umbrellaJoinCtrl.js
@@ -29,7 +29,8 @@
             };
             //添加成员
             $scope.addMember=function(){
-                $state.go("umbrellaMemberAdd",{id:$scope.umbrellaId});
+                // $state.go("umbrellaMemberAdd",{id:$scope.umbrellaId});
+                window.location.href ="../wisdom/umbrella?value="+new Date().getTime()+"#/umbrellaMemberAdd/"+$stateParams.id;
             }
             var compareDate = function (start,end){
                 if(start==null||start.length==0||end==null||end.length==0){
