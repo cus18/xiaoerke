1d03494327aa6ff1e123639f2211c4c2969f1cb3
孙晓 <897909806@qq.com>
2016-06-07 19:57:42 +0800

    保护伞发送消息

--- xiaoerke-wechat/xiaoerke-wechat-service/src/main/java/com/cxqm/xiaoerke/modules/wechat/service/impl/WechatPatientCoreServiceImpl.java
+++ xiaoerke-wechat/xiaoerke-wechat-service/src/main/java/com/cxqm/xiaoerke/modules/wechat/service/impl/WechatPatientCoreServiceImpl.java
@@ -425,7 +425,7 @@ public class WechatPatientCoreServiceImpl implements WechatPatientCoreService {
 			String id = EventKey.split("_")[1];
 			param.put("id",id);
 			List<Map<String,Object>> list = babyUmbrellaInfoService.getBabyUmbrellaInfo(param);
-			String tourl = "";
+			String tourl = "http://s2.xiaork.cn/keeper/wechatInfo/fieldwork/wechat/author?url=http://s2.xiaork.cn/keeper/wechatInfo/getUserWechatMenId?url=umbrellab";
 			if(list1.size()==0){//用户第一次加入保护伞
 				BabyUmbrellaInfo newBabyUmbrellaInfo = new BabyUmbrellaInfo();
 				if(list.size()!=0){
@@ -453,7 +453,7 @@ public class WechatPatientCoreServiceImpl implements WechatPatientCoreService {
 						String url = "";
 						WechatMessageUtil.templateModel(title, keyword1, keyword2, "", "", remark, token, url, fromOpenId, templateId);
 					}
-					if("a".equals(list.get(0).get("version"))&&!EventKey.contains("120000000")){
+					if("a".equals(list.get(0).get("version"))){
 						tourl = "http://s2.xiaork.cn/keeper/wechatInfo/fieldwork/wechat/author?url=http://s2.xiaork.cn/keeper/wechatInfo/getUserWechatMenId?url=umbrellaa";
 						newBabyUmbrellaInfo.setVersion("a");
 						Map<String, Object> result=new HashMap<String, Object>();
@@ -469,8 +469,6 @@ public class WechatPatientCoreServiceImpl implements WechatPatientCoreService {
 						}else {
 							newBabyUmbrellaInfo.setPayResult("fail");
 						}
-					}else{
-						tourl = "http://s2.xiaork.cn/keeper/wechatInfo/fieldwork/wechat/author?url=http://s2.xiaork.cn/keeper/wechatInfo/getUserWechatMenId?url=umbrellab";
 					}
 				}
 				newBabyUmbrellaInfo.setOpenid(toOpenId);
