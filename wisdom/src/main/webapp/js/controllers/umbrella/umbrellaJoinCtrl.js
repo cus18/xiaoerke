0408b81400d71f493893682a43e074e484a41aef
张博 <zbm0915@gmail.com>
2016-06-07 20:43:25 +0800

    保护伞强制登录

--- wisdom/src/main/webapp/js/controllers/umbrella/umbrellaJoinCtrl.js
+++ wisdom/src/main/webapp/js/controllers/umbrella/umbrellaJoinCtrl.js
@@ -61,7 +61,7 @@
                         }
                         $scope.loadShare();
                         
-                        updateActivationTime.save({"id":data.id}, function (data){
+                        updateActivationTime.save({"id":$scope.umbrellaId}, function (data){
                             if(data.result!='1'){
                                 alert("未知错误,请尝试刷新页面");
                             }
