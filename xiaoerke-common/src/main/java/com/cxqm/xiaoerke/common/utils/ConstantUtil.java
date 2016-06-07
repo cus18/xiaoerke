6e745662297718242032cd24186e17770cedd3d8
张博 <zbm0915@gmail.com>
2016-06-07 22:52:05 +0800

    保护伞上线链接修改

--- xiaoerke-common/src/main/java/com/cxqm/xiaoerke/common/utils/ConstantUtil.java
+++ xiaoerke-common/src/main/java/com/cxqm/xiaoerke/common/utils/ConstantUtil.java
@@ -2,13 +2,12 @@ package com.cxqm.xiaoerke.common.utils;
 
 public class ConstantUtil {
 
-	public static final String AMR_TOMP3_FUNC = "windows";
-//	public static final String AMR_TOMP3_FUNC = "linux";
-	public static final String SERVER_ADDRESS = "101.200.194.68";
-	public static final String DOMAIN_VALUE = ".xiaork.com";
-	public static final String AMR_TOMP3_WINDOWSPATH = "C:\\ffmpeg\\ffmpeg\\bin\\";
-	public static final String AMR_TOMP3_WINDOWSPATHTEMP = "C:\\ffmpeg\\temp\\";
+	public static final String AMR_TOMP3_FUNC = "linux";
+	public static final String DOMAIN_VALUE = ".baodf.com";
+	public static final String AMR_TOMP3_WINDOWSPATH = "E:\\ffmpeg\\ffmpeg\\bin\\";
+	public static final String AMR_TOMP3_WINDOWSPATHTEMP = "E:\\ffmpeg\\temp\\";
 	public static final String AMR_TOMP3_LINUXPATH = "/mnt/temp/";
+	public static final String SERVER_ADDRESS = "101.200.194.68";
 
 	//初始化
 	public static String APP_ID = "wx0baf90e904df0117";//微信开发平台应用id
@@ -32,16 +31,22 @@ public class ConstantUtil {
 	public static String NOTIFY_INSURANCE_URL = "http://s68.baodf.com/titan/user/getInsurancePayNotifyInfo";
 	public static String NOTIFY_CUSTOMER_URL = "http://s68.baodf.com/titan/user/getCustomerPayNotifyInfo";
 	public static String NOTIFY_CONSULTPHONE_URL = "http://s68.baodf.com/titan/user/getPhoneConsultPayNotifyInfo";
+	public static String NOTIFY_UMBRELLA_URL = "http://s68.baodf.com/titan/user/getUmbrellaPayNotifyInfo";
 
 	public static int DAY_QUARTER_NUMBER = 96;
 	public static int VISIT_INTERVAL = 15;
 	public static String MARKET_WEB_URL = "http://xiaork.com/";
-	public static String WISDOM_WEB_URL = "http://s165.baodf.com:8081/";
-	//public static String ANGEL_WEB_URL = "http://s34.baodf.com:8081/";
-	public static String KEEPER_WEB_URL = "http://s251.baodf.com:8081/";
+	public static String WISDOM_WEB_URL = "http://s165.baodf.com/";
+	public static String ANGEL_WEB_URL = "http://s132.baodf.com";
+	public static String KEEPER_WEB_URL = "http://s251.baodf.com/";
 	public static String DOCTOR_WEB_URL = "http://s68.baodf.com:8081/";
 	public static String TITAN_WEB_URL = "http://s68.baodf.com/";
 
-//	public static String ANGEL_WEB_URL = "http://xiaork.com/";
-	public static String ANGEL_WEB_URL = "http://xiaoxiaoerke.com";
+//	public static String ANGEL_WEB_URL = "http://s34.baodf.com:8081";
+
+//	public static String ANGEL_WEB_URL = "http://xiaoxiaoerke.com";
+
+
+
+
 }
