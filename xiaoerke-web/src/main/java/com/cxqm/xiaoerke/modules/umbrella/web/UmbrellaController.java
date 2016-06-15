c94b5e54b1da7174d2f7762290bc23ec61fc8f88
孙晓 <897909806@qq.com>
2016-06-15 15:01:26 +0800

    保护伞发送消息

--- xiaoerke-web/src/main/java/com/cxqm/xiaoerke/modules/umbrella/web/UmbrellaController.java
+++ xiaoerke-web/src/main/java/com/cxqm/xiaoerke/modules/umbrella/web/UmbrellaController.java
@@ -160,36 +160,40 @@ public class UmbrellaController  {
         Map parameter = systemService.getWechatParameter();
         String token = (String)parameter.get("token");
         if(list.size()!=0){
-            if((Integer) list.get(0).get("umbrella_money")<400000){
-                String fromOpenId = (String)list.get(0).get("openid");//分享者openid
-                String babyId = (String)list.get(0).get("baby_id");
-
+            String fromOpenId = (String)list.get(0).get("openid");//分享者openid
+            String babyId = (String)list.get(0).get("baby_id");
+            int oldUmbrellaMoney = (Integer) list.get(0).get("umbrella_money");
+            int newUmbrellaMoney = (Integer) list.get(0).get("umbrella_money")+20000;
+            int friendJoinNum = (Integer) list.get(0).get("friendJoinNum");
+            WechatAttention wa = wechatAttentionService.getAttentionByOpenId(toOpenId);
+            String nickName = "";
+            if(wa!=null){
+                nickName = StringUtils.isNotNull(wa.getNickname())?wa.getNickname():"";
+            }
+            String title = "恭喜您，您的好友"+nickName+"已成功加入。您既帮助了朋友，也提升了2万保障金！";
+            String templateId = "b_ZMWHZ8sUa44JrAjrcjWR2yUt8yqtKtPU8NXaJEkzg";
+            String keyword1 = "您已拥有"+newUmbrellaMoney/10000+"万的保障金，还需邀请"+(400000-newUmbrellaMoney)/20000+"位好友即可获得最高40万保障金。";
+            String remark = "邀请一位好友，增加2万保额，最高可享受40万保障！";
+            if(oldUmbrellaMoney<400000){
                 BabyUmbrellaInfo babyUmbrellaInfo = new BabyUmbrellaInfo();
                 babyUmbrellaInfo.setId(Integer.parseInt(fromId));
-                int umbrellaMoney = (Integer) list.get(0).get("umbrella_money")+20000;
-                babyUmbrellaInfo.setUmberllaMoney(umbrellaMoney);
+                babyUmbrellaInfo.setUmberllaMoney(newUmbrellaMoney);
                 babyUmbrellaInfoSerivce.updateBabyUmbrellaInfoById(babyUmbrellaInfo);
-                WechatAttention wa = wechatAttentionService.getAttentionByOpenId(toOpenId);
-                String nickName = "";
-                if(wa!=null){
-                    nickName = StringUtils.isNotNull(wa.getNickname())?wa.getNickname():"";
-                }
-
-                String title = "恭喜您，您的好友"+nickName+"已成功加入。您既帮助了朋友，也提升了2万保障金！";
-                String templateId = "b_ZMWHZ8sUa44JrAjrcjWR2yUt8yqtKtPU8NXaJEkzg";
-                String keyword1 = "您已拥有"+babyUmbrellaInfo.getUmberllaMoney()/10000+"万的保障金，还需邀请"+(400000-umbrellaMoney)/20000+"位好友即可获得最高40万保障金。";
-                String keyword2 = StringUtils.isNotNull(babyId)?"观察期":"待激活";
-                String remark = "邀请一位好友，增加2万保额，最高可享受40万保障！";
-                if(umbrellaMoney == 400000){
-                    title = "感谢您的爱心，第10位好友"+nickName+"已成功加入，一次分享，一份关爱，汇聚微小力量，传递大爱精神！";
-                    templateId = "b_ZMWHZ8sUa44JrAjrcjWR2yUt8yqtKtPU8NXaJEkzg";
-                    keyword1 = "您已成功拥有40万的最高保障金。";
-                    keyword2 = StringUtils.isNotNull(babyId)?"观察期":"待激活";
-                    remark = "您还可以继续邀请好友，传递关爱精神，让更多的家庭拥有爱的保障！";
-                }
-                String url = "http://s251.baodf.com/keeper/wechatInfo/fieldwork/wechat/author?url=http://s251.baodf.com/keeper/wechatInfo/getUserWechatMenId?url=31";
-                WechatMessageUtil.templateModel(title, keyword1, keyword2, "", "", remark, token, url, fromOpenId, templateId);
             }
+            if(newUmbrellaMoney>=400000){
+                title = "感谢您的爱心，第"+(friendJoinNum+1)+"位好友"+nickName+"已成功加入，一次分享，一份关爱，汇聚微小力量，传递大爱精神！";
+                templateId = "b_ZMWHZ8sUa44JrAjrcjWR2yUt8yqtKtPU8NXaJEkzg";
+                keyword1 = "您已成功拥有40万的最高保障金。";
+                remark = "您还可以继续邀请好友，传递关爱精神，让更多的家庭拥有爱的保障！";
+            }
+            BabyUmbrellaInfo babyUmbrellaInfo = new BabyUmbrellaInfo();
+            babyUmbrellaInfo.setId(Integer.parseInt(fromId));
+            babyUmbrellaInfo.setFriendJoinNum(friendJoinNum+1);
+            babyUmbrellaInfoSerivce.updateBabyUmbrellaInfoById(babyUmbrellaInfo);
+
+            String keyword2 = StringUtils.isNotNull(babyId)?"观察期":"待激活";
+            String url = "http://s251.baodf.com/keeper/wechatInfo/fieldwork/wechat/author?url=http://s251.baodf.com/keeper/wechatInfo/getUserWechatMenId?url=31";
+            WechatMessageUtil.templateModel(title, keyword1, keyword2, "", "", remark, token, url, fromOpenId, templateId);
         }
 
         String title = "宝大夫送你一份见面礼";
