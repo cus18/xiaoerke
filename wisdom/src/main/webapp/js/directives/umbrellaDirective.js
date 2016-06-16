5c398c1f24c0213017422ee7985a263898a1d6cb
izhengly <820359064@qq.com>
2016-06-16 17:52:18 +0800

    ionic test demo

--- wisdom/src/main/webapp/js/directives/umbrellaDirective.js
+++ wisdom/src/main/webapp/js/directives/umbrellaDirective.js
@@ -28,5 +28,73 @@ define(['appUmbrella','jquery'], function (app,$) {
                     }
                 }
             }])
+        .directive('swipeCards', ['$rootScope', function($rootScope) {
+            return {
+                restrict: 'E',
+                template: '<div class="swipe-cards" ng-transclude></div>',
+                replace: true,
+                transclude: true,
+                scope: {},
+                controller: function($scope, $element) {
+                    // Instantiate the controller
+                    var swipeController = new SwipeableCardController({
+                    });
+
+                    // We add a root scope event listener to facilitate interacting with the
+                    // directive incase of no direct scope access
+                    $rootScope.$on('swipeCard.pop', function(isAnimated) {
+                        swipeController.popCard(isAnimated);
+                    });
+
+                    // return the object so it is accessible to child
+                    // directives that 'require' this directive as a parent.
+                    return swipeController;
+                }
+            }
+        }])
+        .directive('swipeCard', ['$timeout', function($timeout) {
+            return {
+                restrict: 'E',
+                template: '<div class="swipe-card" ng-transclude></div>',
+
+                // Requiring the swipeCards directive makes the controller available
+                // in the linking function
+                require: '^swipeCards',
+                replace: true,
+                transclude: true,
+                scope: {
+                    onSwipe: '&',
+                    onDestroy: '&'
+                },
+                compile: function(element, attr) {
+                    return function($scope, $element, $attr, swipeCards) {
+                        var el = $element[0];
+
+                        // Instantiate our card view
+                        var swipeableCard = new SwipeableCardView({
+                            el: el,
+                            onSwipe: function() {
+                                $timeout(function() {
+                                    $scope.onSwipe();
+                                });
+                            },
+                            onDestroy: function() {
+                                $timeout(function() {
+                                    $scope.onDestroy();
+                                });
+                            },
+                        });
+
+                        // Make the card available to the parent scope, not necessary
+                        // but makes it easier to interact with (similar to iOS exposing
+                        // parent controllers and views dynamically to children)
+                        $scope.$parent.swipeCard = swipeableCard;
+
+                        // We can push a new card onto the controller card stack, animating it in
+                        swipeCards.pushCard(swipeableCard);
+                    }
+                }
+            }
+        }])
 
 })
