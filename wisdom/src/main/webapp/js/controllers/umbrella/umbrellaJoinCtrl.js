abcc94f9bf0c52c42066443acfcb1dd5253b9762
张博 <zbm0915@gmail.com>
2016-06-21 11:37:40 +0800

    保护伞JS 更新

--- wisdom/src/main/webapp/js/controllers/umbrella/umbrellaJoinCtrl.js
+++ wisdom/src/main/webapp/js/controllers/umbrella/umbrellaJoinCtrl.js
@@ -64,7 +64,7 @@
                     dataType:'json',
                     success:function(data) {
                         if(data.openid=="none"){
-                            // window.location.href = "http://s251.baodf.com/keeper/wechatInfo/fieldwork/wechat/author?url=http://s251.baodf.com/keeper/wechatInfo/getUserWechatMenId?url=umbrellaa";
+                            window.location.href = "http://s251.baodf.com/keeper/wechatInfo/fieldwork/wechat/author?url=http://s251.baodf.com/keeper/wechatInfo/getUserWechatMenId?url=umbrellaa";
                         }
                     },
                     error : function() {
