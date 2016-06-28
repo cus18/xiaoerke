2b8ee85150aff725ce09c555787955809e70063a
chenxiaoqiong <635264100@qq.com>
2016-06-28 18:51:13 +0800

    蛋蛋捐款-接口优化

--- xiaoerke-web/src/main/java/com/cxqm/xiaoerke/modules/mutualHelp/web/mutualHelpDonationController.java
+++ xiaoerke-web/src/main/java/com/cxqm/xiaoerke/modules/mutualHelp/web/mutualHelpDonationController.java
@@ -6,6 +6,7 @@ import com.cxqm.xiaoerke.modules.mutualHelp.entity.MutualHelpDonation;
 import com.cxqm.xiaoerke.modules.mutualHelp.service.MutualHelpDonationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -26,34 +27,34 @@ public class MutualHelpDonationController {
 
     /**
      * 照片墙：我的捐款信息+所有捐款和留言信息
-     * @param paramMap
+     * @param params
      * @return
      */
     @RequestMapping(value = "/photoWall", method = {RequestMethod.POST, RequestMethod.GET})
     @ResponseBody
-    public Map<String, Object> getDetail(HttpServletRequest request, Map<String, Object> paramMap){
-        String openId = (String) paramMap.get("openId");
+    public Map<String, Object> getDetail(HttpServletRequest request, @RequestBody Map<String, Object> params){
+        String openId = (String) params.get("openId");
         if(!StringUtils.isNotNull(openId)){
             openId = CookieUtils.getCookie(request,"openId");
         }
 
         HashMap<String,Object> searchMap = new HashMap<String, Object>();
         searchMap.put("openId", openId);
-        searchMap.put("userId", (Integer) paramMap.get("userId"));
-        searchMap.put("donationType", (Integer) paramMap.get("donationType"));
+        searchMap.put("userId", (Integer) params.get("userId"));
+        searchMap.put("donationType", (Integer) params.get("donationType"));
         return service.getDonatonDetail(searchMap);
     }
 
     /**
      * 捐款总人次
-     * @param paramMap
+     * @param params
      * @return
      */
     @RequestMapping(value = "/count", method = {RequestMethod.POST, RequestMethod.GET})
     @ResponseBody
-    public Map<String, Object> getManTime(Map<String, Object> paramMap){
+    public Map<String, Object> getManTime(@RequestBody Map<String, Object> params){
         Map<String,Object> response = new HashMap<String, Object>();
-        Integer type = (Integer) paramMap.get("donationType");
+        Integer type = (Integer) params.get("donationType");
         response.put("count",service.getCount(type));
 
         return response;
@@ -61,14 +62,14 @@ public class MutualHelpDonationController {
 
     /**
      * 捐款总金额
-     * @param paramMap
+     * @param params
      * @return
      */
     @RequestMapping(value = "/sumMoney", method = {RequestMethod.POST, RequestMethod.GET})
     @ResponseBody
-    public Map<String, Object> getSumMoney(Map<String, Object> paramMap){
+    public Map<String, Object> getSumMoney(@RequestBody Map<String, Object> params){
         Map<String,Object> response = new HashMap<String, Object>();
-        Integer type = (Integer) paramMap.get("donationType");
+        Integer type = (Integer) params.get("donationType");
         response.put("count",service.getCount(type));
 
         return response;
@@ -77,37 +78,37 @@ public class MutualHelpDonationController {
 
     /**
      * 获取最新一条留言
-     * @param paramMap
+     * @param params
      * @return
      */
     @RequestMapping(value = "/lastNote", method = {RequestMethod.POST, RequestMethod.GET})
     @ResponseBody
-    public Map<String, Object> getLastNote(Map<String, Object> paramMap){
+    public Map<String, Object> getLastNote(@RequestBody Map<String, Object> params){
         HashMap<String,Object> searchMap = new HashMap<String, Object>();
-        searchMap.put("donationType", (Integer) paramMap.get("donationType"));
+        searchMap.put("donationType", (Integer) params.get("donationType"));
 
         return service.getLastNote(searchMap);
     }
 
     /**
      * 捐款或留言
-     * @param paramMap
+     * @param params
      * @return
      */
     @RequestMapping(value = "/addNoteAndDonation", method = {RequestMethod.POST, RequestMethod.GET})
     @ResponseBody
-    public Map<String,Object> addNoteAndDonation(HttpServletRequest request, Map<String, Object> paramMap){
-        String openId = (String) paramMap.get("openId");
+    public Map<String,Object> addNoteAndDonation(HttpServletRequest request, @RequestBody Map<String, Object> params){
+        String openId = (String) params.get("openId");
         if(!StringUtils.isNotNull(openId)){
             openId = CookieUtils.getCookie(request,"openId");
         }
 
         MutualHelpDonation mutualHelpDonation = new MutualHelpDonation();
         mutualHelpDonation.setOpenId(openId);
-        mutualHelpDonation.setUserId((String) paramMap.get("userId"));
-        mutualHelpDonation.setMoney((Double) paramMap.get("money"));
-        mutualHelpDonation.setLeaveNote((String) paramMap.get("leaveNote"));
-        mutualHelpDonation.setDonationType((Integer) paramMap.get("donationType"));
+        mutualHelpDonation.setUserId((String) params.get("userId"));
+        mutualHelpDonation.setMoney((Double) params.get("money"));
+        mutualHelpDonation.setLeaveNote((String) params.get("leaveNote"));
+        mutualHelpDonation.setDonationType((Integer) params.get("donationType"));
 
         int n = service.saveNoteAndDonation(mutualHelpDonation);
         Map<String,Object> response = new HashMap<String, Object>();
