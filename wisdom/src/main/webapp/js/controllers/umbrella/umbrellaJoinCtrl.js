6f2fee83e1b1b9306c7e6205ba5c519a70c869ae
张博 <zbm0915@gmail.com>
2016-06-28 19:27:16 +0800

    宝护伞改版

--- wisdom/src/main/webapp/js/controllers/umbrella/umbrellaJoinCtrl.js
+++ wisdom/src/main/webapp/js/controllers/umbrella/umbrellaJoinCtrl.js
@@ -100,6 +100,7 @@
                             }
                         });
                     }else{
+                        $scope.loadShare();
                         $scope.finally=true;
                         $scope.addFamily=true;
                         $scope.umbrellaMoney=data.umbrella.umbrella_money;
@@ -242,7 +243,7 @@
                                 wx.ready(function () {
                                     // 2.2 监听“分享到朋友圈”按钮点击、自定义分享内容及分享结果接口
                                     wx.onMenuShareTimeline({
-                                        title: '5元＝40万？原来做公益，只要一根雪糕钱！', // 分享标题
+                                        title: '5元变成40万,看完我就激动了!', // 分享标题
                                         link:  "http://s202.xiaork.com/wisdom/umbrella#/umbrellaLead/"+$scope.umbrellaId+"/"+$scope.status,
                                         imgUrl: 'http://xiaoerke-healthplan-pic.oss-cn-beijing.aliyuncs.com/umbrella/A8327D229FE265D234984EF57D37EC87.jpg', // 分享图标
                                         success: function (res) {
@@ -265,7 +266,7 @@
                                         }
                                     });
                                     wx.onMenuShareAppMessage({
-                                        title: '5元＝40万？原来做公益，只要一根雪糕钱！', // 分享标题
+                                        title: '5元变成40万,看完我就激动了!', // 分享标题
                                         desc: "我已成为宝护伞互助公益爱心大使，领到了40万的健康保障，你也快来加入吧！", // 分享描述
                                         link:  "http://s202.xiaork.com/wisdom/umbrella#/umbrellaLead/"+$scope.umbrellaId+"/"+$scope.status, // 分享链接
                                         imgUrl: 'http://xiaoerke-healthplan-pic.oss-cn-beijing.aliyuncs.com/umbrella/A8327D229FE265D234984EF57D37EC87.jpg', // 分享图标
