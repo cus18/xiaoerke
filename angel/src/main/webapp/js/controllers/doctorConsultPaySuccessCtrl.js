593716d7c5c329d6df2bc3a79f88f284ea448289
lipangpangz <270255061>
2016-06-30 16:05:20 +0800

    new

--- angel/src/main/webapp/js/controllers/doctorConsultPaySuccessCtrl.js
+++ angel/src/main/webapp/js/controllers/doctorConsultPaySuccessCtrl.js
@@ -2,10 +2,9 @@ angular.module('controllers', ['luegg.directives','ngFileUpload'])
     .controller('doctorConsultPaySuccessCtrl', ['$scope',
         function ($scope) {
             $scope.doctorConsultJumpFirstInit = function () {
-                console.log('aaaaaaa')
-            }
-            $scope.doctorConsultPay = function () {
-                window.location.href="http://localhost:8080/keeper/wxPay/patientPay.do?serviceType=doctorConsultPay"
+            };
+            $scope.closeDoctorConsultPaySuccess = function () {
+                wx.closeWindow();
             };
         }])
 
