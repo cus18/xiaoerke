4b486bd4ff23c2e4cc17251135131170d92ad581
王保伟 <565871712@qq.com>
2016-06-30 17:12:55 +0800

    咨询收费

--- xiaoerke-payment/xiaoerke-payment-service/src/main/java/com/cxqm/xiaoerke/modules/account/service/impl/AccountServiceImpl.java
+++ xiaoerke-payment/xiaoerke-payment-service/src/main/java/com/cxqm/xiaoerke/modules/account/service/impl/AccountServiceImpl.java
@@ -413,6 +413,8 @@ public class AccountServiceImpl implements AccountService {
             parameters.put("notify_url", ConstantUtil.NOTIFY_UMBRELLA_URL);//通知地址
         }else if(serviceType.equals("lovePlanService")){
             parameters.put("notify_url", ConstantUtil.NOTIFY_LOVEPLAN_URL);//通知地址
+        }if(serviceType.equals("doctorConsultPay")){
+            parameters.put("notify_url", ConstantUtil.NOTIFY_LOVEPLAN_URL);//通知地址
         }
         parameters.put("trade_type", "JSAPI");//交易类型
         parameters.put("openid", openId);//用户标示
