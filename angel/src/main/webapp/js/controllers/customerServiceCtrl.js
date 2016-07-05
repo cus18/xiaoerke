e472a1b26a9f88610c044c18b1b38b0d5408b724
王保伟 <565871712@qq.com>
2016-07-05 15:38:58 +0800

    前台只咨询服务

--- angel/src/main/webapp/js/controllers/customerServiceCtrl.js
+++ angel/src/main/webapp/js/controllers/customerServiceCtrl.js
@@ -1,13 +1,16 @@
 angular.module('controllers', ['luegg.directives','ngFileUpload'])
-    .controller('customerServiceCtrl', ['$scope',
-        function ($scope) {
+    .controller('customerServiceCtrl',['$scope','ConsultCustomOnly',
+        function ($scope,ConsultCustomOnly) {
             $scope.customerServiceInit = function () {
             };
             $scope.doctorConsultPay = function () {
                 window.location.href="http://localhost:8080/keeper/wxPay/patientPay.do?serviceType=doctorConsultPay"
             };
             $scope.closeCustomerService = function () {
-                wx.closeWindow();
+
+                ConsultCustomOnly.get({openId:"o8gDqvuFoAJA8WBgfL0H3ZlApe3w"},function () {
+                    wx.closeWindow();
+                })
             };
         }])
 
