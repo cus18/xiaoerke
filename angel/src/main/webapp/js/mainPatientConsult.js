e69421829ccf292b6f21b4410b43133416e671d3
lipangpangz <270255061>
2016-07-11 17:56:52 +0800

    newd

--- angel/src/main/webapp/js/mainPatientConsult.js
+++ angel/src/main/webapp/js/mainPatientConsult.js
@@ -12,7 +12,7 @@ require.config({
         "angular-file-upload": "libs/angular-file-upload.min",
         "angular-file-upload-shim":"libs/angular-file-upload-shim.min",
         "ocLazyLoad":"libs/ocLazyLoad.require.min",
-        "jquery":"libs/jquery-2.1.3.min",
+        //"jquery":"libs/jquery-2.1.3.min",
         "jqueryFace":"libs/jquery.qqFace",
         "browser":"libs/jquery.browser.min",
         "patientConsultFactory" : "services/patientConsultFactory",
@@ -45,13 +45,13 @@ require.config({
             exports: 'angular-sanitize'
         },
         'ocLazyLoad': ['angular'],
-        'appPatientConsult':['ocLazyLoad'],
+        'appPatientConsult':['ocLazyLoad']
     }
 });
 
 require(['angular','angular-resource','angular-sanitize','angular-ui-router',
         'angular-file-upload','angular-file-upload-shim',
-        'ocLazyLoad', 'jquery','jqueryFace','browser','patientConsultFactory','patientConsultDirective',
+        'ocLazyLoad', 'jqueryFace','browser','patientConsultFactory','patientConsultDirective',
         'patientConsultRoute','appPatientConsult'],
     function (angular){
         angular.bootstrap(document,["patientConsultApp"]);
