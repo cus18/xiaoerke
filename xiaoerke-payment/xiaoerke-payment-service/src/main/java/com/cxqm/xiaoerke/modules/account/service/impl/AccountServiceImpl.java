7973a33aab337ccbac5bf2d725437f3ac58c2767
chenxiaoqiong <635264100@qq.com>
2016-07-11 19:18:18 +0800

    lovePlan

--- xiaoerke-payment/xiaoerke-payment-service/src/main/java/com/cxqm/xiaoerke/modules/account/service/impl/AccountServiceImpl.java
+++ xiaoerke-payment/xiaoerke-payment-service/src/main/java/com/cxqm/xiaoerke/modules/account/service/impl/AccountServiceImpl.java
@@ -391,7 +391,11 @@ public class AccountServiceImpl implements AccountService {
         parameters.put("appid", ConstantUtil.APP_ID);//微信服务号的appid
         parameters.put("mch_id", ConstantUtil.PARTNER);//商户号
         parameters.put("nonce_str",noncestr);//随机字符串
-        parameters.put("body", "会员服务费");//描述
+        if(serviceType.equals("lovePlanService")) {
+            parameters.put("body", "爱心捐款");//描述
+        }else{
+            parameters.put("body", "会员服务费");//描述
+        }
         parameters.put("out_trade_no", out_trade_no);//商户订单号
         parameters.put("total_fee", order_price);//金额
         parameters.put("spbill_create_ip",request.getRemoteAddr());//终端ip
