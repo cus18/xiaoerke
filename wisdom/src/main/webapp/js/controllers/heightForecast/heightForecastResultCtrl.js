5dba87e389109ee2a03e748604b2369376c385a9
lipangpangz <270255061>
2016-07-22 16:03:11 +0800

    height

--- wisdom/src/main/webapp/js/controllers/heightForecast/heightForecastResultCtrl.js
+++ wisdom/src/main/webapp/js/controllers/heightForecast/heightForecastResultCtrl.js
@@ -50,27 +50,27 @@ angular.module('controllers', ['ionic']).controller('heightForecastResultCtrl', 
                 if($scope.babyHeight < 170){
                     $scope.babyImg = 'http://xiaoerke-pc-baodf-pic.oss-cn-beijing.aliyuncs.com/height%2Fimages_w_png_03.png';
                     $scope.starImg = 'http://xiaoerke-pc-baodf-pic.oss-cn-beijing.aliyuncs.com/height%2Fphoto_hejiong_png_03.png';
-                    $scope.babyDes = '我家男宝的身高居然和何炅一样'+$scope.babyHeight+'，文质彬彬都说帅，你也来测下！';
+                    $scope.babyDes = '我家男宝的身高居然和何炅差不多'+$scope.babyHeight+'，文质彬彬都说帅，你也来测下！';
                 }else if($scope.babyHeight < 175){
                     $scope.babyImg = 'http://xiaoerke-pc-baodf-pic.oss-cn-beijing.aliyuncs.com/height%2Fimage_q_png_03.png';
                     $scope.starImg = 'http://xiaoerke-pc-baodf-pic.oss-cn-beijing.aliyuncs.com/height%2Fphoto_liangchaowei_png_03.png';
-                    $scope.babyDes = '我家男宝的身高居然和权志龙一样'+$scope.babyHeight+'，气宇轩昂人人夸，你也来测下！';
+                    $scope.babyDes = '我家男宝的身高居然和权志龙差不多'+$scope.babyHeight+'，气宇轩昂人人夸，你也来测下！';
                 }else if($scope.babyHeight < 180){
                     $scope.babyImg = 'http://xiaoerke-pc-baodf-pic.oss-cn-beijing.aliyuncs.com/height%2Fimage_y_png_03.png';
                     $scope.starImg = 'http://xiaoerke-pc-baodf-pic.oss-cn-beijing.aliyuncs.com/height%2Fphoto_songzhongji_png_03.png';
-                    $scope.babyDes = '我家男宝的身高居然和宋仲基一样'+$scope.babyHeight+'，玉树临风全都爱，你也来测下！';
+                    $scope.babyDes = '我家男宝的身高居然和宋仲基差不多'+$scope.babyHeight+'，玉树临风全都爱，你也来测下！';
                 }else if($scope.babyHeight < 185){
                     $scope.babyImg = 'http://xiaoerke-pc-baodf-pic.oss-cn-beijing.aliyuncs.com/height%2Fimage_c_png_03.png';
                     $scope.starImg = 'http://xiaoerke-pc-baodf-pic.oss-cn-beijing.aliyuncs.com/height%2Fphoto_wuyanzu_png_03.png';
-                    $scope.babyDes = '我家男宝的身高居然和吴彦祖一样'+$scope.babyHeight+'，长腿欧巴超羡慕，你也来测下！';
+                    $scope.babyDes = '我家男宝的身高居然和吴彦祖差不多'+$scope.babyHeight+'，长腿欧巴超羡慕，你也来测下！';
                 }else if($scope.babyHeight < 190){
                     $scope.babyImg = 'http://xiaoerke-pc-baodf-pic.oss-cn-beijing.aliyuncs.com/height%2Fimage_d_png_03.png';
                     $scope.starImg = 'http://xiaoerke-pc-baodf-pic.oss-cn-beijing.aliyuncs.com/height%2Fphoto_wuyifan_png_03.png';
-                    $scope.babyDes = '我家男宝的身高居然和吴亦凡一样'+$scope.babyHeight+'，顶天立地大气概，你也来测下！';
+                    $scope.babyDes = '我家男宝的身高居然和吴亦凡差不多'+$scope.babyHeight+'，顶天立地大气概，你也来测下！';
                 }else{
                     $scope.babyImg = 'http://xiaoerke-pc-baodf-pic.oss-cn-beijing.aliyuncs.com/height%2Fimage_l_png_03.png';
                     $scope.starImg = 'http://xiaoerke-pc-baodf-pic.oss-cn-beijing.aliyuncs.com/height%2Fphoto_yijianlian_png_03.png';
-                    $scope.babyDes = '我家男宝的身高居然和易建联一样'+$scope.babyHeight+'，篮球飞人绝对帅，你也来测下！';
+                    $scope.babyDes = '我家男宝的身高居然和易建联差不多'+$scope.babyHeight+'，篮球飞人绝对帅，你也来测下！';
                 }
             }
             if($stateParams.resultGirl != '' && $stateParams.resultBoy == 0){
@@ -80,19 +80,19 @@ angular.module('controllers', ['ionic']).controller('heightForecastResultCtrl', 
                 if($scope.babyHeight < 160){
                     $scope.babyImg = 'http://xiaoerke-pc-baodf-pic.oss-cn-beijing.aliyuncs.com/height%2Fimage_x_png_03.png';
                     $scope.starImg = 'http://xiaoerke-pc-baodf-pic.oss-cn-beijing.aliyuncs.com/height%2Fphoto_ciayilin_png_03.png';
-                    $scope.babyDes = '我家女宝的身高居然和蔡依林一样'+$scope.babyHeight+'，小家碧玉惹人爱，你也来测下！';
+                    $scope.babyDes = '我家女宝的身高居然和蔡依林差不多'+$scope.babyHeight+'，小家碧玉惹人爱，你也来测下！';
                 }else if($scope.babyHeight < 165){
                     $scope.babyImg = 'http://xiaoerke-pc-baodf-pic.oss-cn-beijing.aliyuncs.com/height%2Fimage_dajiaguixiu_png_03_03.png';
                     $scope.starImg = 'http://xiaoerke-pc-baodf-pic.oss-cn-beijing.aliyuncs.com/height%2Fphoto_sunli_png_03.png';
-                    $scope.babyDes = '我家女宝的身高居然和孙俪一样'+$scope.babyHeight+'，大家闺秀人人爱，你也来测下！';
+                    $scope.babyDes = '我家女宝的身高居然和孙俪差不多'+$scope.babyHeight+'，大家闺秀人人爱，你也来测下！';
                 }else if($scope.babyHeight < 170){
                     $scope.babyImg = 'http://xiaoerke-pc-baodf-pic.oss-cn-beijing.aliyuncs.com/height%2Fimages_png_03.png';
                     $scope.starImg = 'http://xiaoerke-pc-baodf-pic.oss-cn-beijing.aliyuncs.com/height%2Fphoto_yangmi_png_03.png';
-                    $scope.babyDes = '我家女宝的身高居然和杨幂一样'+$scope.babyHeight+'，窈窕淑女君好逑，你也来测下！';
+                    $scope.babyDes = '我家女宝的身高居然和杨幂差不多'+$scope.babyHeight+'，窈窕淑女君好逑，你也来测下！';
                 }else{
                     $scope.babyImg = 'http://xiaoerke-pc-baodf-pic.oss-cn-beijing.aliyuncs.com/height%2Fimage_chaoji_03.png';
                     $scope.starImg = 'http://xiaoerke-pc-baodf-pic.oss-cn-beijing.aliyuncs.com/height%2Fphoto_lin_png_03.png';
-                    $scope.babyDes = '我家女宝的身高居然和林志玲一样'+$scope.babyHeight+'，维密超模大赢家，你也来测下！';
+                    $scope.babyDes = '我家女宝的身高居然和林志玲差不多'+$scope.babyHeight+'，维密超模大赢家，你也来测下！';
                 }
             }
             loadShare();
