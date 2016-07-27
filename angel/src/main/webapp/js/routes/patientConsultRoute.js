4c1330fe1dd5ea1eadb79d9e83d680fff6f6b419
lipangpangz <270255061>
2016-07-27 11:55:54 +0800

    qq表情

--- angel/src/main/webapp/js/routes/patientConsultRoute.js
+++ angel/src/main/webapp/js/routes/patientConsultRoute.js
@@ -31,6 +31,7 @@ define(['appPatientConsult'], function(app){
                                 loadFunction($templateCache, $ocLazyLoad, $q, $http,'patientConsultFirstCtrl',
                                     ['js/controllers/patientConsultFirstCtrl.js',
                                         'js/libs/scrollglue.js','js/libs/moment.min.js',
+                                        'js/libs/jquery.qqFace.js',
                                         'js/styles/patientConsultFirst.css'],
                                     'js/views/patientConsultFirst.html?ver='+patientConsultVersion);
                             }
@@ -48,7 +49,7 @@ define(['appPatientConsult'], function(app){
                                 loadFunction($templateCache, $ocLazyLoad, $q, $http,'patientConsultUmbrellaCtrl',
                                     ['js/controllers/patientConsultUmbrellaCtrl.js',
                                         'js/libs/scrollglue.js','js/libs/moment.min.js',
-                                        'js/libs/scrollglue.js','js/libs/moment.min.js',
+                                        'js/libs/jquery.qqFace.js',
                                         'js/styles/patientConsultUmbrella.css'],
                                     'js/views/patientConsultUmbrella.html?ver='+patientConsultVersion);
                             }
