21d082b254c1b146bae507fe7396893cdd40b6e8
cjk18 <cus18@sina.com>
2016-08-01 12:59:38 +0800

    new merge

--- titan/src/main/webapp/js/services/olympicBabyFactory.js
+++ titan/src/main/webapp/js/services/olympicBabyFactory.js
@@ -1,15 +1,52 @@
-/**
- * 取得实际位置
- * 调用方式：geoFactory.getGeo();
- */
-var user_h5 = ''
-var user_appoint = ''
-var wxChat = 'wechatInfo/'
 
 define(['appOlympicBaby'], function (app) {
     app
-        .factory('ListHospitalDoctor',['$resource',function ($resource){
-            return $resource(user_h5 + 'consultPhoneDoctor/getDoctorListByHospital');
+        .factory('GetGameMemberNum',['$resource',function ($resource){
+            /**
+             * 获取参加奥运宝宝游戏的总人数
+             * inputParam: {}
+             * resultParam: {gameMemberNum:8888}
+             ***/
+            return $resource('olympicBaby/firstPage/GetGameMemberNum');
+        }])
+        .factory('GetGameMemberStatus',['$resource',function ($resource){
+            /**
+             * 获取参加用户目前所处的游戏关数,已经要开启下一关，所要进行的操作
+             * input: {openid:"fwefwefewfw"}
+             * result: {gameLevel:2,gameAction:1，needInviteFriendNum:3,}
+             * gameLevel 表示当前所处的管卡,0表示用户处于通关的状态
+             * gameAction 1表示需要邀请用户关注，2表示需要邀请好友
+             * needInviteFriendNum 表示还需要邀请加入的好友数
+             ***/
+            return $resource('olympicBaby/firstPage/GetGameMemberNum');
+        }])
+        .factory('GetGameMemberStatus',['$resource',function ($resource){
+            /**
+             * 获取参加用户目前所处的游戏关数,已经要开启下一关，所要进行的操作
+             * input: {openid:"fwefwefewfw"}
+             * result: {gameLevel:2,gameAction:1，needInviteFriendNum:3,}
+             * gameLevel表示当前所处的管卡
+             * gameAction 1表示需要邀请用户关注，2表示需要邀请好友
+             * needInviteFriendNum 表示还需要邀请加入的好友数
+             ***/
+            return $resource('olympicBaby/firstPage/GetGameMemberNum');
+        }])
+        .factory('GetFirstPageSlideMessage',['$resource',function ($resource){
+            /**
+             * 获取奖品领取的轮播图消息
+             * result: {messageList:[{userName:孙晓,Content:"获得了微波炉大奖"},{userName:孙晓,Content:"获得了微波炉大奖"},
+             * {userName:孙晓,Content:"获得了微波炉大奖"},{userName:孙晓,Content:"获得了微波炉大奖"},
+             * {userName:孙晓,Content:"获得了微波炉大奖"}]}
+             ***/
+            return $resource('olympicBaby/firstPage/GetFirstPageSlideMessage');
+        }])
+        .factory('GetUserGameScore',['$resource',function ($resource){
+            /**
+             * 获取用户的游戏积分
+             * input: {openid:"fwefwefewfw"}
+             * result: {gameScore:8888}
+             ***/
+            return $resource('olympicBaby/firstPage/GetUserGameScore');
         }])
 
 
