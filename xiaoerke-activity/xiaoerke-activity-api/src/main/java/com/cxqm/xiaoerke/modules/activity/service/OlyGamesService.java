4c20e4f3d697ccee22e61d26cdb4e35daf6bb24a
zhaodeliang <1975063953@qq.com>
2016-08-01 17:25:16 +0800

    test

---
+++ xiaoerke-activity/xiaoerke-activity-api/src/main/java/com/cxqm/xiaoerke/modules/activity/service/OlyGamesService.java
@@ -0,0 +1,8 @@
+package com.cxqm.xiaoerke.modules.activity.service;
+
+/**
+ * Created by deliang on 16/8/01.
+ */
+public interface OlyGamesService {
+
+}
---
+++ xiaoerke-activity/xiaoerke-activity-service/src/main/java/com/cxqm/xiaoerke/modules/activity/service/impl/OlyGamesServiceImpl.java
@@ -0,0 +1,15 @@
+package com.cxqm.xiaoerke.modules.activity.service.impl;
+
+import com.cxqm.xiaoerke.modules.activity.service.OlyGamesService;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by deliang on 16/03/18.
+ */
+
+@Service
+public class OlyGamesServiceImpl implements OlyGamesService {
+
+
+
+}
