a7c0995fdfabbdef1923427376694de7702d3f2a
zhaodeliang <1975063953@qq.com>
2016-08-01 17:13:16 +0800

    test

---
+++ xiaoerke-activity/xiaoerke-activity-service/src/main/java/com/cxqm/xiaoerke/modules/activity/dao/OlyBabyGamesDao.java
@@ -0,0 +1,18 @@
+package com.cxqm.xiaoerke.modules.activity.dao;
+
+import com.cxqm.xiaoerke.common.persistence.annotation.MyBatisDao;
+import com.cxqm.xiaoerke.modules.activity.entity.OlyBabyGamesVo;
+@MyBatisDao
+public interface OlyBabyGamesDao {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(OlyBabyGamesVo record);
+
+    int insertSelective(OlyBabyGamesVo record);
+
+    OlyBabyGamesVo selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(OlyBabyGamesVo record);
+
+    int updateByPrimaryKey(OlyBabyGamesVo record);
+}
