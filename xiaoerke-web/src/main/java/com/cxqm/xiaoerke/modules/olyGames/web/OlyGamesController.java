53df6e6037357dbd8f9d3dee88ee264b3350763e
zhaodeliang <1975063953@qq.com>
2016-08-01 17:11:25 +0800

    123123

---
+++ xiaoerke-web/src/main/java/com/cxqm/xiaoerke/modules/olyGames/web/OlyGamesController.java
@@ -0,0 +1,19 @@
+package com.cxqm.xiaoerke.modules.olyGames.web;
+
+import com.cxqm.xiaoerke.common.web.BaseController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+/**
+ * 奥运 Controller
+ *
+ * @author deliang
+ * @version 2016-08-01
+ */
+@Controller
+@RequestMapping(value = "olyGames")
+public class OlyGamesController extends BaseController {
+
+
+}
