7c9c72375412eae55e6bd3ae7f5e55ab8c21dca5
zhouguangyue <zhouGUANGYUE666>
2016-08-02 18:33:45 +0800

    update

--- titan/src/main/webapp/js/controllers/olympicBaby/olympicBabyDrawPrizeCtrl.js
+++ titan/src/main/webapp/js/controllers/olympicBaby/olympicBabyDrawPrizeCtrl.js
@@ -1,6 +1,6 @@
 ﻿angular.module('controllers', []).controller('olympicBabyDrawPrizeCtrl', [
-        '$scope','$state','$timeout',
-        function ($scope,$state,$timeout) {
+        '$scope','$state','$timeout','GetGameScorePrize',
+        function ($scope,$state,$timeout,GetGameScorePrize) {
             $scope.title = "奥运宝贝-抽奖";
             $scope.layerLock = false;//浮层总开关
             $scope.noScoreLock = false;//无积分浮层开关
@@ -9,10 +9,25 @@
             $scope.DrawPriseLock = false;//抽奖浮层开关
             $scope.prizeArray = [false,false,false,false,false,false,false,false,false,false];//奖品列表图片
             var click=false;
+            var openid = "111111" ;
+            var prizIndex;
 
             //页面初始化
             $scope.olympicBabyDrawPrizeInit = function(){
                 lottery.init('lottery');
+                //获取用户抽奖内容
+                /*GetGameScorePrize.save({"openid":openid},function (data) {
+                    console.log("data",data);
+                    var index = parseInt(data.prizeOrder);
+                    if(index==3){
+                        prizIndex = 3;
+                    }else if(index>9){
+                        prizIndex = 5;
+                    }else{
+                        prizIndex = index;
+                    }
+                });*/
+
             };
 
             var lottery={
@@ -24,39 +39,23 @@
                 cycle:50,	//转动基本次数：即至少需要转动多少次再进入抽奖环节
                 prize:-1,	//中奖位置
                 init:function(id){
-                    /*if (this.count>0) {
-                     $lottery = $("#"+id);
-                     //$units = $lottery.find(".lottery-unit");
-                     this.obj = $lottery;
-                     // console.log("$lottery " + $lottery);
-                     //this.count = $units.length;
-                     //$lottery.find(".lottery-unit-"+this.index).addClass("active");
-                     $scope.prizeArray[this.index]=true;
-                     console.log("init");
-                     };*/
                     if ($("#"+id).find(".lottery-unit").length>0) {
-                        $lottery = $("#"+id);
-                        $units = $lottery.find(".lottery-unit");
-                        this.obj = $lottery;
-                        this.count = $units.length;
+                        var lotteryId = $("#"+id);
+                        var lotteryUnits = lotteryId.find(".lottery-unit");
+                        this.obj = lotteryId;
+                        this.count = lotteryUnits.length;
                     }
-
                 },
 
                 littleRoll:function(){
                     var index = this.index;
                     var count = this.count;
-                    // var lottery = this.obj;
-                    //$(lottery).find(".lottery-unit-"+index).removeClass("active");
                     $scope.prizeArray[index] = false;
-                    console.log(index+" "+$scope.prizeArray[index]);
                     index += 1;
                     if (index>count-1) {
                         index = 0;
                     };
-                    //$(lottery).find(".lottery-unit-"+index).addClass("active");
                     $scope.prizeArray[index] = true;
-                    console.log(index+" "+$scope.prizeArray[index]);
                     this.index=index;
                     return false;
                 },
@@ -88,12 +87,20 @@
                     lottery.prize=-1;
                     lottery.times=0;
                     click=false;
+                    /*if(prizIndex == 3){
+                        $scope.noPriseLock = true;
+                        $scope.layerLock = true;
+                    }else{
+                        $scope.getPriseLock = true;
+                        $scope.layerLock = true;
+                    }*/
                 }else{
                     if (lottery.times<lottery.cycle) {
                         lottery.speed -= 10;
                     }else if(lottery.times==lottery.cycle) {
-                        var index = Math.random()*(lottery.count)|0;
-                        lottery.prize = index;
+                         var index = Math.random()*(lottery.count)|0;
+                         lottery.prize = index;
+                        //lottery.prize = prizIndex;
                         console.log("我的奖品"+ lottery.prize );
                     }else{
                         if (lottery.times > lottery.cycle+10 && ((lottery.prize==0 && lottery.index==7) || lottery.prize==lottery.index+1)) {
@@ -104,7 +111,7 @@
                     }
                     if (lottery.speed<40) {
                         lottery.speed=40;
-                    };
+                    }
                     console.log(lottery.times+'^^^^^^'+lottery.speed+'^^^^^^^'+lottery.prize);
                     //lottery.timer = setTimeout(roll,lottery.speed);
                     lottery.timer = $timeout(function () {
@@ -166,7 +173,7 @@
 
             //点击 浮层下 领取奖品
             $scope.getPrize = function(){
-                $state.go("olympicBabyFirst");
+
             };
-            
+
     }]);
