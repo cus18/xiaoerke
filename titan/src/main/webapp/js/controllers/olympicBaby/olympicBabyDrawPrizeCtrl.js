21c368a0f20270d1af24f449c184d586eba9d7fe
zhouguangyue <zhouGUANGYUE666>
2016-08-02 16:45:35 +0800

    update

--- titan/src/main/webapp/js/controllers/olympicBaby/olympicBabyDrawPrizeCtrl.js
+++ titan/src/main/webapp/js/controllers/olympicBaby/olympicBabyDrawPrizeCtrl.js
@@ -7,60 +7,125 @@
             $scope.noPriseLock = false;//无奖品浮层开关
             $scope.getPriseLock = false;//有奖品浮层开关
             $scope.DrawPriseLock = false;//抽奖浮层开关
+            $scope.prizeArray = [false,false,false,false,false,false,false,false,false,false];//奖品列表图片
+            var click=false;
 
-            //奖品列表图片
-         /*   $scope.prizeArray = [
-                {
-                    check:false,
-                    show: "http://xiaoerke-remain-pic.oss-cn-beijing.aliyuncs.com/olympicBaby/drawPrize/prize1_n.png",
+            //页面初始化
+            $scope.olympicBabyDrawPrizeInit = function(){
+                lottery.init('lottery');
+            };
+
+            var lottery={
+                index:-1,	//当前转动到哪个位置，起点位置
+                count:10,	//总共有多少个位置
+                timer:0,	//setTimeout的ID，用clearTimeout清除
+                speed:20,	//初始转动速度
+                times:0,	//转动次数
+                cycle:50,	//转动基本次数：即至少需要转动多少次再进入抽奖环节
+                prize:-1,	//中奖位置
+                init:function(id){
+                    /*if (this.count>0) {
+                     $lottery = $("#"+id);
+                     //$units = $lottery.find(".lottery-unit");
+                     this.obj = $lottery;
+                     // console.log("$lottery " + $lottery);
+                     //this.count = $units.length;
+                     //$lottery.find(".lottery-unit-"+this.index).addClass("active");
+                     $scope.prizeArray[this.index]=true;
+                     console.log("init");
+                     };*/
+                    if ($("#"+id).find(".lottery-unit").length>0) {
+                        $lottery = $("#"+id);
+                        $units = $lottery.find(".lottery-unit");
+                        this.obj = $lottery;
+                        this.count = $units.length;
+                    }
+
                 },
-                {
-                    show: "http://xiaoerke-remain-pic.oss-cn-beijing.aliyuncs.com/olympicBaby/drawPrize/prize2_n.png",
-                    check: "http://xiaoerke-remain-pic.oss-cn-beijing.aliyuncs.com/olympicBaby/drawPrize/prize2_y.png"
+
+                littleRoll:function(){
+                    var index = this.index;
+                    var count = this.count;
+                    // var lottery = this.obj;
+                    //$(lottery).find(".lottery-unit-"+index).removeClass("active");
+                    $scope.prizeArray[index] = false;
+                    console.log(index+" "+$scope.prizeArray[index]);
+                    index += 1;
+                    if (index>count-1) {
+                        index = 0;
+                    };
+                    //$(lottery).find(".lottery-unit-"+index).addClass("active");
+                    $scope.prizeArray[index] = true;
+                    console.log(index+" "+$scope.prizeArray[index]);
+                    this.index=index;
+                    return false;
                 },
-                {
-                    show: "http://xiaoerke-remain-pic.oss-cn-beijing.aliyuncs.com/olympicBaby/drawPrize/prize3_n.png",
-                    check: "http://xiaoerke-remain-pic.oss-cn-beijing.aliyuncs.com/olympicBaby/drawPrize/prize3_y.png"
-                },
-                {
-                    show: "http://xiaoerke-remain-pic.oss-cn-beijing.aliyuncs.com/olympicBaby/drawPrize/prize4_n.png",
-                    check: "http://xiaoerke-remain-pic.oss-cn-beijing.aliyuncs.com/olympicBaby/drawPrize/prize4_y.png"
-                },
-                {
-                    show: "http://xiaoerke-remain-pic.oss-cn-beijing.aliyuncs.com/olympicBaby/drawPrize/prize5_n.png",
-                    check: "http://xiaoerke-remain-pic.oss-cn-beijing.aliyuncs.com/olympicBaby/drawPrize/prize5_y.png"
-                },
-                {
-                    show: "http://xiaoerke-remain-pic.oss-cn-beijing.aliyuncs.com/olympicBaby/drawPrize/prize6_n.png",
-                    check: "http://xiaoerke-remain-pic.oss-cn-beijing.aliyuncs.com/olympicBaby/drawPrize/prize6_y.png"
-                },
-                {
-                    show: "http://xiaoerke-remain-pic.oss-cn-beijing.aliyuncs.com/olympicBaby/drawPrize/prize7_n.png",
-                    check: "http://xiaoerke-remain-pic.oss-cn-beijing.aliyuncs.com/olympicBaby/drawPrize/prize7_y.png"
-                },
-                {
-                    show: "http://xiaoerke-remain-pic.oss-cn-beijing.aliyuncs.com/olympicBaby/drawPrize/prize8_n.png",
-                    check: "http://xiaoerke-remain-pic.oss-cn-beijing.aliyuncs.com/olympicBaby/drawPrize/prize8_y.png"
-                },
-                {
-                    show: "http://xiaoerke-remain-pic.oss-cn-beijing.aliyuncs.com/olympicBaby/drawPrize/prize9_n.png",
-                    check: "http://xiaoerke-remain-pic.oss-cn-beijing.aliyuncs.com/olympicBaby/drawPrize/prize9_y.png"
-                },
-                {
-                    show: "http://xiaoerke-remain-pic.oss-cn-beijing.aliyuncs.com/olympicBaby/drawPrize/prize10_n.png",
-                    check: "http://xiaoerke-remain-pic.oss-cn-beijing.aliyuncs.com/olympicBaby/drawPrize/prize10_y.png"
+                stop:function(index){
+                    this.prize=index;
+                    return false;
                 }
-            ];*/
-            $scope.prizeArray = [false,false,false,false,false,false,false,false,false,false];
+            };
+
+            //点击 我要抽奖
+            $scope.startDrawPrize = function(){
+                if (click) {
+                    return false;
+                }else{
+                    lottery.speed=100;
+                    roll();
+                    click=true;
+                    return false;
+                }
+
+            };
+
+            var roll=function (){
+                lottery.times += 1;
+                lottery.littleRoll();
+                if (lottery.times > lottery.cycle+10 && lottery.prize==lottery.index) {
+                    //clearTimeout(lottery.timer);
+                    $timeout.cancel(lottery.timer);
+                    lottery.prize=-1;
+                    lottery.times=0;
+                    click=false;
+                }else{
+                    if (lottery.times<lottery.cycle) {
+                        lottery.speed -= 10;
+                    }else if(lottery.times==lottery.cycle) {
+                        var index = Math.random()*(lottery.count)|0;
+                        lottery.prize = index;
+                        console.log("我的奖品"+ lottery.prize );
+                    }else{
+                        if (lottery.times > lottery.cycle+10 && ((lottery.prize==0 && lottery.index==7) || lottery.prize==lottery.index+1)) {
+                            lottery.speed += 110;
+                        }else{
+                            lottery.speed += 20;
+                        }
+                    }
+                    if (lottery.speed<40) {
+                        lottery.speed=40;
+                    };
+                    console.log(lottery.times+'^^^^^^'+lottery.speed+'^^^^^^^'+lottery.prize);
+                    //lottery.timer = setTimeout(roll,lottery.speed);
+                    lottery.timer = $timeout(function () {
+                        roll();
+                    },lottery.speed);
+                }
+                return false;
+            }
+
             //点击 返回游戏
             $scope.backHomePage = function(){
                 $state.go("olympicBabyFirst");
             };
+
             //点击 查看我的奖品
             $scope.lookMyPrize = function(){
-                $scope.layerLock = true;
-                $scope.getPriseLock = true;
+                // $scope.layerLock = true;
+                // $scope.getPriseLock = true;
+                $state.go("olympicBabyMyPrize");
             };
+
             //点击 查看抽奖规则
             var myScroll;
             $scope.lookDrawPrizeRule = function(){
@@ -87,114 +152,21 @@
                 $scope.getPriseLock = false;//有奖品浮层开关
                 $scope.DrawPriseLock = false;//抽奖浮层开关
             };
-            //点
+
             //点击 浮层下 玩游戏
             $scope.playGame = function(){
                 $state.go("olympicBabyFirst");
             };
+
             //点击 浮层下 再玩一次
             $scope.tryAgain = function(){
-                $state.go("olympicBabyFirst");
+                //$state.go("olympicBabyFirst");
+                $scope.cancelLayer();
             };
+
             //点击 浮层下 领取奖品
             $scope.getPrize = function(){
                 $state.go("olympicBabyFirst");
             };
-
-            /*  抽奖函数定义*/
-            var click=false;
-            var lottery={
-                    index:-1,	//当前转动到哪个位置，起点位置
-                    count:10,	//总共有多少个位置
-                    timer:0,	//setTimeout的ID，用clearTimeout清除
-                    speed:20,	//初始转动速度
-                    times:0,	//转动次数
-                    cycle:50,	//转动基本次数：即至少需要转动多少次再进入抽奖环节
-                    prize:-1,	//中奖位置
-                    init:function(id){
-                        if (this.count>0) {
-                            $lottery = $("#"+id);
-                            //$units = $lottery.find(".lottery-unit");
-                            this.obj = $lottery;
-                           // console.log("$lottery " + $lottery);
-                            //this.count = $units.length;
-                            //$lottery.find(".lottery-unit-"+this.index).addClass("active");
-                            $scope.prizeArray[this.index]=true;
-                            console.log("init");
-                        };
-                    },
-
-                    littleRoll:function(){
-                        var index = this.index;
-                        var count = this.count;
-                       // var lottery = this.obj;
-                        //$(lottery).find(".lottery-unit-"+index).removeClass("active");
-                        $scope.prizeArray[index]=false;
-                       //console.log(index+" "+$scope.prizeArray[index]);
-                        index += 1;
-                        if (index>count-1) {
-                            index = 0;
-                        };
-                        //$(lottery).find(".lottery-unit-"+index).addClass("active");
-                        $scope.prizeArray[index]=true;
-                      //  console.log(index+" "+$scope.prizeArray[index]);
-                        this.index=index;
-                        return false;
-                    },
-                    stop:function(index){
-                        this.prize=index;
-                        return false;
-                    }
-                };
-            var roll=function (){
-                lottery.times += 1;
-                lottery.littleRoll();
-                if (lottery.times > lottery.cycle+10 && lottery.prize==lottery.index) {
-                    clearTimeout(lottery.timer);
-                    lottery.prize=-1;
-                    lottery.times=0;
-                    click=false;
-                }else{
-                    if (lottery.times<lottery.cycle) {
-                        lottery.speed -= 10;
-                    }else if(lottery.times==lottery.cycle) {
-                        var index = Math.random()*(lottery.count)|0;
-                        lottery.prize = index;
-                        console.log("我的奖品"+ lottery.prize );
-                    }else{
-                        if (lottery.times > lottery.cycle+10 && ((lottery.prize==0 && lottery.index==7) || lottery.prize==lottery.index+1)) {
-                            lottery.speed += 110;
-                        }else{
-                            lottery.speed += 20;
-                        }
-                    }
-                    if (lottery.speed<40) {
-                        lottery.speed=40;
-                    };
-                    //console.log(lottery.times+'^^^^^^'+lottery.speed+'^^^^^^^'+lottery.prize);
-                    lottery.timer = setTimeout(roll,lottery.speed);
-                }
-                return false;
-            }
-
-
-            //点击 我要抽奖
-            $scope.startDrawPrize = function(){
-                if (click) {
-                    return false;
-                }else{
-                    lottery.speed=100;
-                    roll();
-                    click=true;
-                    return false;
-                }
-                //lottery.stop();
-
-            };
-
-            //页面初始化
-            $scope.olympicBabyDrawPrizeInit = function(){
-                lottery.init('lottery');
-            };
             
-    }])
+    }]);
