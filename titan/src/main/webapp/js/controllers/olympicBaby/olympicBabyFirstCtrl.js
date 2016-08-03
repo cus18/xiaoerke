ddf406547869ade49029e51cf3a53d3a5424d578
lipangpangz <270255061>
2016-08-03 11:12:08 +0800

    游戏首页获取openId

--- titan/src/main/webapp/js/controllers/olympicBaby/olympicBabyFirstCtrl.js
+++ titan/src/main/webapp/js/controllers/olympicBaby/olympicBabyFirstCtrl.js
@@ -2,7 +2,22 @@
         '$scope','$state','$timeout',
         function ($scope,$state,$timeout) {
             $scope.title = "奥运宝贝-首页";
-
+            //获取openId
+            /*
+            * $.ajax({
+             url:"umbrella/getOpenid",// 跳转到 action
+             async:true,
+             type:'post',
+             cache:false,
+             dataType:'json',
+             success:function(data) {
+             if(data.openid=="none"){
+             window.location.href = "http://s251.baodf.com/keeper/wechatInfo/fieldwork/wechat/author?url=http://s251.baodf.com/keeper/wechatInfo/getUserWechatMenId?url=37";
+             }
+             },
+             error : function() {
+             }
+             });*/
 
             
     }])
