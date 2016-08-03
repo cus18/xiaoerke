8079d15c0fdf0523f19a80cd9968a56b04eb4906
zhouguangyue <zhouGUANGYUE666>
2016-08-03 16:31:47 +0800

    update

--- titan/src/main/webapp/js/controllers/olympicBaby/olympicBabyMyPrizeCtrl.js
+++ titan/src/main/webapp/js/controllers/olympicBaby/olympicBabyMyPrizeCtrl.js
@@ -1,22 +1,23 @@
 ﻿angular.module('controllers', []).controller('olympicBabyMyPrizeCtrl', [
-        '$scope','$state','$timeout',
-        function ($scope,$state,$timeout) {
-            $scope.title = "奥运宝贝-我的奖品";
-            $scope.showPrize = true;
-            $scope.showNoPrize = false;
+        '$scope','$state',
+        function ($scope,$state) {
+            $scope.showPrize = false;
+            $scope.showNoPrize = true;
             $scope.prizeList = [
                 {
                     src:'http://xiaoerke-pc-baodf-pic.oss-cn-beijing.aliyuncs.com/dkf%2Fconsult%2Fyonghumoren.png',
-                    prizeName:'京东优惠券',
-                    prize:'京东优惠券'
+                    prizeName:'价值168元的乐普多种维生素加矿物质片',
+                    
                 },{
                     src:'http://xiaoerke-pc-baodf-pic.oss-cn-beijing.aliyuncs.com/dkf%2Fconsult%2Fyonghumoren.png',
                     prizeName:'京东优惠券',
-                    prize:'京东优惠券'
+                    
                 },{
                     src:'http://xiaoerke-pc-baodf-pic.oss-cn-beijing.aliyuncs.com/dkf%2Fconsult%2Fyonghumoren.png',
                     prizeName:'京东优惠券',
-                    prize:'京东优惠券'
+                   
                 }
             ]
-    }])
+
+
+    }]);
