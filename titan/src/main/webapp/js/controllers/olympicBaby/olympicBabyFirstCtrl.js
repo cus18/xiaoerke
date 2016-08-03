47706b99efc995fe285995d37c10228b20cb078a
meixiaopeng <210639649@qq.com>
2016-08-03 13:45:55 +0800

    index push

--- titan/src/main/webapp/js/controllers/olympicBaby/olympicBabyFirstCtrl.js
+++ titan/src/main/webapp/js/controllers/olympicBaby/olympicBabyFirstCtrl.js
@@ -18,6 +18,34 @@
              error : function() {
              }
              });*/
+            $scope.olympicBabyFirstInit = function(){
 
-            
+            };
+            function ScrollImgLeft() {
+                var speed = 50;
+                var scroll_begin = document.getElementById("scroll_begin");
+                var scroll_end = document.getElementById("scroll_end");
+                var scroll_div = document.getElementById("scroll_div");
+                scroll_end.innerHTML = scroll_begin.innerHTML;
+
+                function Marquee() {
+                    if (scroll_end.offsetWidth - scroll_div.scrollLeft <= 0) {
+                        scroll_div.scrollLeft -= scroll_begin.offsetWidth;
+                    } else {
+                        scroll_div.scrollLeft++;
+                    }
+                }
+                var MyMar = setInterval(Marquee, speed);
+            };
+            $scope.load= function(){
+                ScrollImgLeft();
+                $('#ruleBtn').bind('click', function() {
+                    $('#ruleBox').show();
+                });
+                $('#ruleBox').bind('click', function(e) {
+                    if (e.target.className != 'ruleBoxContent') {
+                        $('#ruleBox').hide();
+                    }
+                });
+            }
     }])
