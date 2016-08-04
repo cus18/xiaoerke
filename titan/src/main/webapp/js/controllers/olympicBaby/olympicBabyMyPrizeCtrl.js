a5138f7e7df30972488f5cde6637dd0b233a1388
zhouguangyue <zhouGUANGYUE666>
2016-08-04 10:20:06 +0800

    update

--- titan/src/main/webapp/js/controllers/olympicBaby/olympicBabyMyPrizeCtrl.js
+++ titan/src/main/webapp/js/controllers/olympicBaby/olympicBabyMyPrizeCtrl.js
@@ -3,21 +3,6 @@
         function ($scope,$state,GetUserPrizes,GetUserOpenId) {
             $scope.showPrize = false;
             $scope.showNoPrize = true;
-            $scope.prizeList = [
-                {
-                    src:'http://xiaoerke-pc-baodf-pic.oss-cn-beijing.aliyuncs.com/dkf%2Fconsult%2Fyonghumoren.png',
-                    prizeName:'价值168元的乐普多种维生素加矿物质片',
-                    
-                },{
-                    src:'http://xiaoerke-pc-baodf-pic.oss-cn-beijing.aliyuncs.com/dkf%2Fconsult%2Fyonghumoren.png',
-                    prizeName:'京东优惠券',
-                    
-                },{
-                    src:'http://xiaoerke-pc-baodf-pic.oss-cn-beijing.aliyuncs.com/dkf%2Fconsult%2Fyonghumoren.png',
-                    prizeName:'京东优惠券',
-                   
-                }
-            ]
 
             //获取用户的openid
             GetUserOpenId.get(function (data) {
