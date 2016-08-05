ea1fe148eab4d37acfb4225c82aaaecc3299b668
lipangpangz <270255061>
2016-08-05 11:11:24 +0800

    游戏界面

--- titan/src/main/webapp/js/controllers/olympicBaby/olympicBabyFirstCtrl.js
+++ titan/src/main/webapp/js/controllers/olympicBaby/olympicBabyFirstCtrl.js
@@ -94,7 +94,7 @@
                     if($scope.attentionOrNot == 1){
                         $scope.shareFloat = true;
                     }else if($scope.attentionOrNot == 2){
-                        $state.go("olympicBabyInvitationCard",{});
+                        $state.go("olympicBabyInvitationCard",{gameLevel:'2'});
                     }else if($scope.playCount < 3 && $scope.attentionOrNot == 0){
                         recordLogs("action_olympic_baby_tiwce_visit");
                         $state.go("olympicGameLevel2",{});
@@ -113,7 +113,7 @@
                     if($scope.attentionOrNot == 1){
                         $scope.shareFloat = true;
                     }else if($scope.attentionOrNot == 2){
-                        $state.go("olympicBabyInvitationCard",{});
+                        $state.go("olympicBabyInvitationCard",{gameLevel:'3'});
                     }else if($scope.playCount < 3 && $scope.attentionOrNot == 0){
                         recordLogs("action_olympic_baby_thirth_visit");
                         $state.go("olympicGameLevel3",{});
