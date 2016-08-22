65d6c5b44443dfef849081b89d2aa3e5e3e52498
王保伟 <565871712@qq.com>
2016-08-22 15:17:18 +0800

    日志点

--- keeper/src/main/webapp/js/pay/doctorConsultPay.js
+++ keeper/src/main/webapp/js/pay/doctorConsultPay.js
@@ -43,11 +43,11 @@ var doRefresh = function(){
     });
 
      $('#money').html(moneys);
-    recordLogs("consult_chargetest_once_information");
+    recordLogs("consult_charge_twice_information_payclick");
 };
 
 function wechatPay() {
-    recordLogs("consult_chargetest_once_paypage_paybutton");
+    recordLogs("consult_charge_twice_paypage_paybutton");
     var u = navigator.userAgent, app = navigator.appVersion;
     var isAndroid = u.indexOf('Android') > -1 || u.indexOf('linux') > -1; //g
     var isIOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios终端
