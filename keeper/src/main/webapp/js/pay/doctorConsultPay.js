fd798db9cbafc96446d53c1bad6b657b8eea8670
meixiaopeng <210639649@qq.com>
2016-09-12 17:15:37 +0800

    change

--- keeper/src/main/webapp/js/pay/doctorConsultPay.js
+++ keeper/src/main/webapp/js/pay/doctorConsultPay.js
@@ -1,6 +1,7 @@
 var payLock = true;
 var moneys = 9.9;
 var leaveNotes = "reward";
+var useBabyCoin = true;
 
 //页面初始化执行,用户初始化页面参数信息以及微信的支付接口
 var doRefresh = function(){
@@ -46,6 +47,9 @@ var doRefresh = function(){
     recordLogs("consult_charge_twice_information_payclick");
 };
 function wechatPay() {
+    if(useBabyCoin){
+        UseBobyCoinPay();
+    }
     recordLogs("consult_charge_twice_paypage_paybutton");
     var u = navigator.userAgent, app = navigator.appVersion;
     var isAndroid = u.indexOf('Android') > -1 || u.indexOf('linux') > -1; //g
@@ -113,33 +117,61 @@ var recordLogs = function(val){
     });
 };
 (function(){
-    var useBabyCoin = true,
-        time1,
+    var time1,
         bar = 'Choose';
     time1 = setInterval(flag,100);
     function flag(){
         if($('#useBabyCoinRight')){
-            $('#useBabyCoinRight').click(function(){
-                if(useBabyCoin){
-                    bar = 'Unchoose';
-                    useBabyCoin = false;
-                }else{
-                    bar = 'Choose';
-                    useBabyCoin = true;
-                };
-                $('#useBabyCoinRightImg').css('background','url("http://xiaoerke-pc-baodf-pic.oss-cn-beijing.aliyuncs.com/invite/useBabyCoinRight'+bar+'.png") 100% 100%/100% 100%')
+            $.ajax({
+                type: "get",
+                url: 'http://'+window.location.host + "/titan/babyCoin/babyCoinInit",
+                dataType: "json",
+                success: function(data) {
+                    var canUse = 90;
+                    data.babyCoinVo.cash>99?canUse = 99:canUse = data.babyCoinVo.cash;
+                    $('#useBabyCoinLeft').html('宝宝币：'+data.babyCoinVo.cash+'枚，本次可以用'+canUse+'枚');
+                    if(data.babyCoinVo.cash > 0){
+                        $('#useBabyCoinRight').click(function(){
+                            if(useBabyCoin){
+                                bar = 'Unchoose';
+                                useBabyCoin = false;
+                            }else{
+                                bar = 'Choose';
+                                useBabyCoin = true;
+                            };
+                            $('#useBabyCoinRightImg').css('background','url("http://xiaoerke-pc-baodf-pic.oss-cn-beijing.aliyuncs.com/invite/useBabyCoinRight'+bar+'.png") 100% 100%/100% 100%');
+                            console.log(useBabyCoin)
+                        });
+                    }else{
+                        $('#useBabyCoinRightImg').css('background','url("http://xiaoerke-pc-baodf-pic.oss-cn-beijing.aliyuncs.com/invite/useBabyCoinRightUnchoose.png") 100% 100%/100% 100%')
+                        useBabyCoin = false;
+                        console.log(useBabyCoin)
+                    }
+                },
+                error: function(jqXHR){
+                    console.log("发生错误：" + jqXHR.status);
+                },
             });
-            clearTimeout(time1);
         }
+        clearTimeout(time1);
     }
+}());
+//doctorConsultPaySuccess
+function UseBobyCoinPay() {
     $.ajax({
-        type: "get",
-        url: 'http://'+window.location.host + "/titan/babyCoin/babyCoinInit",
+        type: "GET",
+        url: "babyCoin/minusOrAddBabyCoin",
+        async:false,
         dataType: "json",
         success: function(data) {
+            if(data.status == 'success'){
+                console.log('我成功扣到钱了');
+                window.location.href="http://s132.baodf.com/angel/patient/consult#/doctorConsultPaySuccess";
+            }
         },
         error: function(jqXHR){
             console.log("发生错误：" + jqXHR.status);
         },
     });
-}());
+}
+
