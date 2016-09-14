4dd14ab81c82849b2eafb33b5397e2ee1e074721
zhaodeliang <1975063953@qq.com>
2016-09-14 19:28:19 +0800

    single

--- angel/src/main/webapp/js/controllers/patientConsultInviteNewCtrl.js
+++ angel/src/main/webapp/js/controllers/patientConsultInviteNewCtrl.js
@@ -1,17 +1,22 @@
 angular.module('controllers', [])
-    .controller('patientConsultInviteNewCtrl',['$scope','CreateInviteCard',
-        function ($scope,CreateInviteCard) {
-            $scope.inviteNewInit = function(){
-                CreateInviteCard.save({},function(data){
-                    $scope.headImgUrl = {
-                        'background' : 'url('+data.headImgUrl+')',
-                        'background-size' : '100% 100%'
-                    };
-                    $scope.headImgNickName = data.babyCoinVo.nickName;
-                    $scope.userQRCode = {
-                        'background' : 'url('+data.userQRCode+')',
-                        'background-size' : '100% 100%'
-                    };
-                });
-            }
+    .controller('patientConsultInviteNewCtrl', ['$scope', 'CreateInviteCard',
+        function ($scope, CreateInviteCard) {
+
+            var reg = new RegExp("(^|&)" + "oldOpenId" + "=([^&]*)(&|$)", "i");
+            var r = window.location.search.substr(1).match(reg);
+            var oldOpenId = unescape(r[2])
+            alert(oldOpenId);
+            //var marketer = GetQueryString("marketer");
+            alert(marketer);
+            CreateInviteCard.save({}, function (data) {
+                $scope.headImgUrl = {
+                    'background': 'url(' + data.headImgUrl + ')',
+                    'background-size': '100% 100%'
+                };
+                $scope.headImgNickName = data.babyCoinVo.nickName;
+                $scope.userQRCode = {
+                    'background': 'url(' + data.userQRCode + ')',
+                    'background-size': '100% 100%'
+                };
+            });
         }])
