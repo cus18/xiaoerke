7d5281e79c056aff07bac7c075a6d60defa419b4
王保伟 <565871712@qq.com>
2016-09-20 16:23:22 +0800

    结束会话发送消息

--- xiaoerke-web/src/main/java/com/cxqm/xiaoerke/modules/wechat/web/WechatUserController.java
+++ xiaoerke-web/src/main/java/com/cxqm/xiaoerke/modules/wechat/web/WechatUserController.java
@@ -3,6 +3,8 @@ package com.cxqm.xiaoerke.modules.wechat.web;
 import com.cxqm.xiaoerke.common.utils.SignUtil;
 import com.cxqm.xiaoerke.common.utils.WechatUtil;
 import com.cxqm.xiaoerke.common.web.BaseController;
+import com.cxqm.xiaoerke.modules.sys.service.MongoDBService;
+import com.cxqm.xiaoerke.modules.wechat.entity.SpecificChannelRuleVo;
 import com.cxqm.xiaoerke.modules.wechat.entity.SysWechatAppintInfoVo;
 import com.cxqm.xiaoerke.modules.wechat.service.WechatAttentionService;
 import com.cxqm.xiaoerke.modules.wechat.service.WechatPatientCoreService;
@@ -16,6 +18,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -34,6 +37,9 @@ public class WechatUserController extends BaseController {
 
     @Autowired
     private WechatAttentionService wechatAttentionService;
+
+    @Autowired
+    private MongoDBService<SpecificChannelRuleVo> specificChannelRuleMongoDBService;
 
 
     /**
@@ -85,5 +91,35 @@ public class WechatUserController extends BaseController {
         return map;
     }
 
+    /**
+     *根据openid查询关注用户信息
+     */
+    @RequestMapping(value = "/patient/getAttentionInfosss", method = {RequestMethod.POST, RequestMethod.GET})
+    public
+    @ResponseBody
+    Map addsomeparam(HttpSession session, HttpServletRequest request) {
 
+
+        SpecificChannelRuleVo v = new SpecificChannelRuleVo();
+        v.setId("1");
+        v.setQrcode("YY0017");
+        v.setBabyCoin(1000l);
+        v.setDocuments("亲爱的百度同学你好！你可以获得1000枚宝宝币（价值100元，年底前有效哈）直接咨询三甲医院医生哦！");
+        v.setRepeatdocuments("亲爱的百度同学，你的1000枚宝宝币已经领过了哦~  直接咨询就可以啦");
+        v.setEndTime(new Date());
+        specificChannelRuleMongoDBService.insert(v);
+        return null;
+    }
+
+    /**
+     *更新关键字回复的缓存
+     *
+     */
+    @RequestMapping(value = "/patient/updateKeyWordRecovery", method = {RequestMethod.POST, RequestMethod.GET})
+    public
+    @ResponseBody
+    Map updateKeyWordRecovery(HttpSession session, HttpServletRequest request) {
+        wechatPatientCoreService.updateKeywordRecovery();
+        return null;
+    }
 }
