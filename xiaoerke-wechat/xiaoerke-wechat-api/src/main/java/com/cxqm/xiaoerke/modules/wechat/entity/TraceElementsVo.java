629a1e1ba19aadd1a1cb5e6ff242494c6773bf79
王保伟 <565871712@qq.com>
2016-10-10 16:07:07 +0800

    结束会话发送消息

---
+++ xiaoerke-wechat/xiaoerke-wechat-api/src/main/java/com/cxqm/xiaoerke/modules/wechat/entity/TraceElementsVo.java
@@ -0,0 +1,55 @@
+package com.cxqm.xiaoerke.modules.wechat.entity;
+
+import java.util.Date;
+
+public class TraceElementsVo {
+    private Integer id;
+
+    private String openid;
+
+    private String userNo;
+
+    private String resultsUrl;
+
+    private Date createTime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getOpenid() {
+        return openid;
+    }
+
+    public void setOpenid(String openid) {
+        this.openid = openid;
+    }
+
+    public String getUserNo() {
+        return userNo;
+    }
+
+    public void setUserNo(String userNo) {
+        this.userNo = userNo;
+    }
+
+    public String getResultsUrl() {
+        return resultsUrl;
+    }
+
+    public void setResultsUrl(String resultsUrl) {
+        this.resultsUrl = resultsUrl;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+}
