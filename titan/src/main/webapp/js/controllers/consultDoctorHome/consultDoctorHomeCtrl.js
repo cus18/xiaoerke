e8b1a7a50842c7bbe1f9f635251cae4236589035
cus18 <cus18@sina.com>
2016-10-11 14:29:22 +0800

    23

--- titan/src/main/webapp/js/controllers/consultDoctorHome/consultDoctorHomeCtrl.js
+++ titan/src/main/webapp/js/controllers/consultDoctorHome/consultDoctorHomeCtrl.js
@@ -40,10 +40,25 @@ angular.module('controllers', ['ionic']).controller('consultDoctorHomeCtrl', [
                     $scope.doctorImg = doctorImg;
                 }
 
-                BabyCoinInit.save({},function(data){
+                /*BabyCoinInit.save({},function(data){
                     $scope.babyMoney = data.babyCoinVo.cash;
                     $scope.babyCoinRecordVos = data.babyCoinRecordVos;
-                });
+                });*/
+
+                var recordLogs = function(val){
+                    $.ajax({
+                        url:"util/recordLogs",// 跳转到 action
+                        async:true,
+                        type:'get',
+                        data:{logContent:encodeURI(val)},
+                        cache:false,
+                        dataType:'json',
+                        success:function(data) {
+                        },
+                        error : function() {
+                        }
+                    });
+                };
 
                 recordLogs("clickDetails");
 
