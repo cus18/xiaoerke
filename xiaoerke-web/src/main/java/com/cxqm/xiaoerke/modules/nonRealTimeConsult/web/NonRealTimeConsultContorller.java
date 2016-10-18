21656903fc37879e941ab87d976c3e400860480a
zhaodeliang <1975063953@qq.com>
2016-10-18 10:59:59 +0800

    angel

---
+++ xiaoerke-web/src/main/java/com/cxqm/xiaoerke/modules/nonRealTimeConsult/web/NonRealTimeConsultContorller.java
@@ -0,0 +1,49 @@
+package com.cxqm.xiaoerke.modules.nonRealTimeConsult.web;
+
+import com.cxqm.xiaoerke.modules.consult.service.ConsultSessionPropertyService;
+import com.cxqm.xiaoerke.modules.consult.service.SessionRedisCache;
+import com.cxqm.xiaoerke.modules.sys.service.SystemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Created by Administrator on 2016/09/05 0024.
+ */
+@Controller
+@RequestMapping(value = "nonRealTimeConsult")
+public class NonRealTimeConsultContorller {
+
+
+    @Autowired
+    private SessionRedisCache sessionRedisCache;
+
+    @Autowired
+    private ConsultSessionPropertyService consultSessionPropertyService;
+
+    @Autowired
+    private SystemService systemService;
+
+
+
+
+    /**
+     * 获取宝宝币，以及详细记录
+     *
+     * @param request
+     * @param
+     * @return
+     */
+    @RequestMapping(value = "/test", method = {RequestMethod.POST, RequestMethod.GET})
+    @ResponseBody
+    public void test(HttpSession session, HttpServletRequest request) {
+//        babyCoinInit(session, request);
+//        getBabyCoinInfo(session, request);
+    }
+
+}
