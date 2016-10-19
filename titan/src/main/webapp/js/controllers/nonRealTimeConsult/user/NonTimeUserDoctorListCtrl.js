e1739aaf7bab7e989ee656810a4c38feceabe109
lipangpangz <270255061>
2016-10-19 15:30:45 +0800

    非实时用户端

--- titan/src/main/webapp/js/controllers/nonRealTimeConsult/user/NonTimeUserDoctorListCtrl.js
+++ titan/src/main/webapp/js/controllers/nonRealTimeConsult/user/NonTimeUserDoctorListCtrl.js
@@ -3,5 +3,35 @@ angular.module('controllers', []).controller('NonTimeUserDoctorListCtrl', [
         function ($scope,$state,$timeout,$http) {
             $scope.NonTimeUserDoctorListInit = function(){
 
-            }
+            };
+            $scope.goDoctorHomepage = function(){
+
+            };
+            $scope.doctorList = [
+                {
+                    src:'http://xiaoerke-pc-baodf-pic.oss-cn-beijing.aliyuncs.com/dkf%2Fconsult%2Fyonghumoren.png',
+                    doctorName:'梁医生',
+                    department:'妇科',
+                    position:'副主任医师',
+                    description:'擅长：婴儿湿疹，婴儿湿疹，婴儿湿疹，手足口',
+                    population:'9999',
+                    evaluate:'99%'
+                },{
+                    src:'http://xiaoerke-pc-baodf-pic.oss-cn-beijing.aliyuncs.com/dkf%2Fconsult%2Fyonghumoren.png',
+                    doctorName:'梁医生',
+                    department:'妇科',
+                    position:'副主任医师',
+                    description:'擅长：婴儿湿疹，婴儿湿疹，婴儿湿疹，手足口',
+                    population:'9999',
+                    evaluate:'99%'
+                },{
+                    src:'http://xiaoerke-pc-baodf-pic.oss-cn-beijing.aliyuncs.com/dkf%2Fconsult%2Fyonghumoren.png',
+                    doctorName:'梁医生',
+                    department:'妇科',
+                    position:'副主任医师',
+                    description:'擅长：婴儿湿疹，婴儿湿疹，婴儿湿疹，手足口',
+                    population:'9999',
+                    evaluate:'99%'
+                }
+            ];
     }]);
