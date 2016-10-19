a71924211396ab5ad6b03c4b9a60bdee95929a67
王保伟 <565871712@qq.com>
2016-10-19 11:06:45 +0800

    结束会话发送消息

--- xiaoerke-nonRealtimeConsult/xiaoerke-nonRealtimeConsult-api/src/main/java/com/cxqm/xiaoerke/modules/nonRealTimeConsult/service/NonRealTimeConsultService.java
+++ xiaoerke-nonRealtimeConsult/xiaoerke-nonRealtimeConsult-api/src/main/java/com/cxqm/xiaoerke/modules/nonRealTimeConsult/service/NonRealTimeConsultService.java
@@ -1,8 +1,12 @@
 package com.cxqm.xiaoerke.modules.nonRealTimeConsult.service;
 
+import com.cxqm.xiaoerke.modules.consult.entity.ConsultDoctorDepartmentVo;
+import com.cxqm.xiaoerke.modules.consult.entity.ConsultDoctorInfoVo;
 import com.cxqm.xiaoerke.modules.nonRealTimeConsult.entity.NonRealTimeConsultRecordVo;
 import com.cxqm.xiaoerke.modules.nonRealTimeConsult.entity.NonRealTimeConsultSessionVo;
+import com.cxqm.xiaoerke.modules.sys.entity.BabyBaseInfoVo;
 
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -13,4 +17,42 @@ public interface NonRealTimeConsultService {
     List<NonRealTimeConsultSessionVo> selectByNonRealTimeConsultSessionVo(NonRealTimeConsultSessionVo realTimeConsultSessionVo);
 
     List<NonRealTimeConsultRecordVo> selectSessionRecordByVo(NonRealTimeConsultRecordVo nonRealTimeConsultRecordVo);
+
+    /**
+     * 查找所有科室
+     *
+     * */
+
+    List<ConsultDoctorDepartmentVo> departmentList();
+
+    /**
+     * 查找所有明星医生
+     * */
+    List<ConsultDoctorInfoVo> starDoctorList();
+
+    /**
+     * 查找所有明星医生主页资料
+     * */
+    HashMap<String,Object> starDoctorInfo(String userId);
+
+
+    /**
+     * 获取用户宝宝信息
+     * */
+    BabyBaseInfoVo babyBaseInfo(String openid);
+
+    /**
+     * 创建会话。保存宝宝问诊信息
+     * */
+    void createSession(String csUserId,String openid,String content);
+
+    /**
+     * 每天更新医生的咨询量 以及好评度
+     * */
+    void updateConsultDoctorInfo();
+
+    /**
+     * 患者医生间的咨询消息
+     * */
+    void savenConsultRecord(Integer sessionId,String toUser,String fromUser,String content,String type);
 }
