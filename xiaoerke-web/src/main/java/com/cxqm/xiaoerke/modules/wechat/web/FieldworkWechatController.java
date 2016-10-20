bab93116914d106656b9f1188ab38e934c592e11
王保伟 <565871712@qq.com>
2016-10-20 14:32:46 +0800

    结束会话发送消息

--- xiaoerke-web/src/main/java/com/cxqm/xiaoerke/modules/wechat/web/FieldworkWechatController.java
+++ xiaoerke-web/src/main/java/com/cxqm/xiaoerke/modules/wechat/web/FieldworkWechatController.java
@@ -224,6 +224,9 @@ public class FieldworkWechatController {
         }else if("39".equals(url)){
             //非及时咨询
             url = sysPropertyVoWithBLOBsVo.getAngelWebUrl() + "angel/patient/consult#/patientConsultNoFee";
+        }else if("40".equals(url)){
+            //非及时咨询
+            url = sysPropertyVoWithBLOBsVo.getTitanWebUrl() + "titan/nonRealTimeConsult#/NonTimeUserFindDoctor";
         }
         String get_access_token_url = "https://api.weixin.qq.com/sns/oauth2/access_token?" +
                 "appid=APPID" +
