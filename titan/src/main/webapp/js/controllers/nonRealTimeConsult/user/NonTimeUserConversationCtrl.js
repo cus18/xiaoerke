4a5d69f8e0c972068de6c46ff834e382786ca926
王保伟 <565871712@qq.com>
2016-10-21 14:25:31 +0800

    结束会话发送消息

--- titan/src/main/webapp/js/controllers/nonRealTimeConsult/user/NonTimeUserConversationCtrl.js
+++ titan/src/main/webapp/js/controllers/nonRealTimeConsult/user/NonTimeUserConversationCtrl.js
@@ -1,8 +1,13 @@
 angular.module('controllers', []).controller('NonTimeUserConversationCtrl', [
-        '$scope','$state','$timeout','$http',
-        function ($scope,$state,$timeout,$http) {
-            $scope.NonTimeUserConversationInit = function(){
+        '$scope','$state','$stateParams','$timeout','$http','ConversationInfo',
+        function ($scope,$state,$stateParams,$timeout,$http,ConversationInfo) {
+            $scope.pageData =[]
 
+            $scope.NonTimeUserConversationInit = function(){
+                ConversationInfo.save({sessionId:$stateParams.sessionId},function (data) {
+                    console.log(data)
+                    $scope.pageData = data
+                })
             }
             $scope.glued = true;
     }]);
