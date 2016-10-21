fec65ce1fd49d5172c154a34c048442771867cf0
王保伟 <565871712@qq.com>
2016-10-21 16:11:18 +0800

    结束会话发送消息

--- titan/src/main/webapp/js/controllers/nonRealTimeConsult/user/NonTimeUserConversationCtrl.js
+++ titan/src/main/webapp/js/controllers/nonRealTimeConsult/user/NonTimeUserConversationCtrl.js
@@ -1,6 +1,6 @@
 angular.module('controllers', []).controller('NonTimeUserConversationCtrl', [
-        '$scope','$state','$stateParams','$timeout','$http','ConversationInfo',
-        function ($scope,$state,$stateParams,$timeout,$http,ConversationInfo) {
+        '$scope','$state','$stateParams','$timeout','$http','ConversationInfo','UpadateRecorde',
+        function ($scope,$state,$stateParams,$timeout,$http,ConversationInfo,UpadateRecorde) {
             $scope.pageData =[]
 
             $scope.NonTimeUserConversationInit = function(){
@@ -10,4 +10,39 @@ angular.module('controllers', []).controller('NonTimeUserConversationCtrl', [
                 })
             }
             $scope.glued = true;
+            $scope.msgType= "text";
+            $scope.content = "";
+
+            //发送消息
+            $scope.sendMsg = function(){
+                var information = {
+                    "sessionid":$stateParams.sessionId,
+                    "content": $scope.content,
+                    "msgType": $scope.msgType
+                };
+                UpadateRecorde.save(information,function (data) {
+                    if(data.state == "error"){
+                        alert("请重新打开页面提交信息");
+                    }
+                })
+            }
+
+            //提交图片
+            $scope.uploadFiles = function($files,fileType) {
+                console.log('dataJsonValue');
+                for (var i = 0; i < $files.length; i++) {
+                    var file = $files[i];
+                    $scope.upload = $upload.upload({
+                        url: 'nonRealTimeConsultUser/uploadMediaFile',
+                        file: file
+                    }).progress(function(evt) {
+                        console.log('percent: ' + parseInt(100.0 * evt.loaded / evt.total));
+                    }).success(function(data, status, headers, config){
+                        $scope.msgType= "img";
+                        $scope.content = data.imgPath
+                        $scope.sendMsg()
+                    });
+                }
+            };
+
     }]);
