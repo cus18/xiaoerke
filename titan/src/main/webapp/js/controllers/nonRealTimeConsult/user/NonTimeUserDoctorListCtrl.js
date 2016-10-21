c6add1bdf88c230aa4bcf4b12705cf6c886eccf4
王保伟 <565871712@qq.com>
2016-10-21 16:05:23 +0800

    结束会话发送消息

--- titan/src/main/webapp/js/controllers/nonRealTimeConsult/user/NonTimeUserDoctorListCtrl.js
+++ titan/src/main/webapp/js/controllers/nonRealTimeConsult/user/NonTimeUserDoctorListCtrl.js
@@ -9,33 +9,6 @@ angular.module('controllers', []).controller('NonTimeUserDoctorListCtrl', [
                     $scope.doctorList = data.departmentVoList
                 })
             };
-            // $scope.doctorList = [
-            //     {
-            //         src:'http://xiaoerke-pc-baodf-pic.oss-cn-beijing.aliyuncs.com/dkf%2Fconsult%2Fyonghumoren.png',
-            //         doctorName:'梁医生',
-            //         department:'妇科',
-            //         position:'副主任医师',
-            //         description:'擅长：婴儿湿疹，婴儿湿疹，婴儿湿疹，手足口',
-            //         population:'9999',
-            //         evaluate:'99%'
-            //     },{
-            //         src:'http://xiaoerke-pc-baodf-pic.oss-cn-beijing.aliyuncs.com/dkf%2Fconsult%2Fyonghumoren.png',
-            //         doctorName:'梁医生',
-            //         department:'妇科',
-            //         position:'副主任医师',
-            //         description:'擅长：婴儿湿疹，婴儿湿疹，婴儿湿疹，手足口',
-            //         population:'9999',
-            //         evaluate:'99%'
-            //     },{
-            //         src:'http://xiaoerke-pc-baodf-pic.oss-cn-beijing.aliyuncs.com/dkf%2Fconsult%2Fyonghumoren.png',
-            //         doctorName:'梁医生',
-            //         department:'妇科',
-            //         position:'副主任医师',
-            //         description:'擅长：婴儿湿疹，婴儿湿疹，婴儿湿疹，手足口',
-            //         population:'9999',
-            //         evaluate:'99%'
-            //     }
-            // ];
             $scope.checkDoctorInformation = function(index){
                 console.log($scope.doctorList[index].doctorId);
                 location.href="consultDoctorHome#/consultDoctorHome/"+$scope.doctorList[index].userId
