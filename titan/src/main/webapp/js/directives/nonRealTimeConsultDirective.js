9d597ee466062385520b44867a33f19ee2a4155b
王保伟 <565871712@qq.com>
2016-10-25 16:48:54 +0800

    结束会话发送消息

--- titan/src/main/webapp/js/directives/nonRealTimeConsultDirective.js
+++ titan/src/main/webapp/js/directives/nonRealTimeConsultDirective.js
@@ -49,5 +49,16 @@ define(['appNonRealTimeConsult','jquery'], function (app,$) {
                 }
             }
         }])
+        .directive('errSrc', function() {
+            return {
+                link: function(scope, element, attrs) {
+                    element.bind('error', function() {
+                        if (attrs.src != attrs.errSrc) {
+                            attrs.$set('src', attrs.errSrc);
+                        }
+                    });
+                }
+            }
+        })
 
 })
