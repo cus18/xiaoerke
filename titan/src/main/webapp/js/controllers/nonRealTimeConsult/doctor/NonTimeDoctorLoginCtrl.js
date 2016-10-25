a68c124a217d0fdb32cf59d3b0924c3a92766a7f
zhaodeliang <1975063953@qq.com>
2016-10-25 11:41:26 +0800

    angel

--- titan/src/main/webapp/js/controllers/nonRealTimeConsult/doctor/NonTimeDoctorLoginCtrl.js
+++ titan/src/main/webapp/js/controllers/nonRealTimeConsult/doctor/NonTimeDoctorLoginCtrl.js
@@ -12,4 +12,55 @@ angular.module('controllers', []).controller('NonTimeDoctorLoginCtrl', [
                     });
             };
 
+            var getValidateCode = function(){
+                if(countdown<60)
+                {
+                    return;
+                }
+                else{
+                    var partner = /^1[3578]\d{9}$/;
+                    if(!partner.exec($('#username').val())) {
+                        $('#info').html("手机号格式不对");
+                        return;
+                    }
+                    else{
+                        var mydata = '{"userPhone":"'
+                            + $('#username').val() + '"}';
+                        $.ajax({
+                            url:"util/user/getCode",
+                            async:true,
+                            type:'post',
+                            data:mydata,
+                            cache:false,
+                            contentType:"application/json; charset=utf-8",
+                            dataType:'json',
+                            success:function(data) {
+
+                            },
+                            error : function() {
+                            }
+                        });
+                        lockValidateCode();
+                    }
+                }
+            }
+
+            var countdown=60;
+            var lockValidateCode = function()
+            {
+                if (countdown == 0) {
+                    $('#validateCode').html("获取验证码");
+                    countdown = 60;
+                    return;
+                } else {
+                    $('#validateCode').html("重新发送(" + countdown + ")");
+                    countdown--;
+                }
+                setTimeout(function() {
+                        lockValidateCode() }
+                    ,1000)
+            }
+
+
+
     }]);
