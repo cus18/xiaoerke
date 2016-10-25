d8416d896d137183ae2122766e1598b916fce656
izhengly <820359064@qq.com>
2016-10-25 19:18:05 +0800

    nonTimeConsult doctor

--- titan/src/main/webapp/js/controllers/nonRealTimeConsult/doctor/NonTimeDoctorMessageListCtrl.js
+++ titan/src/main/webapp/js/controllers/nonRealTimeConsult/doctor/NonTimeDoctorMessageListCtrl.js
@@ -1,30 +1,36 @@
 angular.module('controllers', []).controller('NonTimeDoctorMessageListCtrl', [
     '$scope', '$state', '$timeout', '$http', 'GetDoctorLoginStatus','GetDoctorService',
     function ($scope, $state, $timeout, $http, GetDoctorLoginStatus,GetDoctorService) {
-
-        //校验是否登陆
-        GetDoctorLoginStatus.save({}, function (data) {
-            $scope.pageLoading = false;
-            if (data.status == "failure") {
-                window.location.href = "http://127.0.0.1/titan/nonRealTimeConsult#/NonTimeDoctorLogin";
-            } else {
-                $scope.MessageListInfo = $scope.GetMessageList("currentService");//currentService 当前服务 allService 全部服务
-            }
-        });
+        $scope.selectItem = "cur";
         $scope.GetMessageList = function (item) {
             GetDoctorService.save({serviceType:item}, function (data) {
-                $scope.MessageList = data.ListInfo;
+                $scope.curService = data.ListInfo;
+                console.log("111",data.ListInfo);
             });
         };
-
         $scope.selectService = function (item) {
             $scope.selectItem = item;
         };
-
-
         //页面初始化
         $scope.NonTimeDoctorMessageListInit = function () {
+            //校验是否登陆
+            GetDoctorLoginStatus.save({}, function (data) {
+                $scope.pageLoading = false;
+                if (data.status == "failure") {
+                    window.location.href = "http://127.0.0.1/titan/nonRealTimeConsult#/NonTimeDoctorLogin";
+                } else {
+                   // $scope.MessageListInfo = $scope.GetMessageList("currentService");//currentService 当前服务 allService 全部服务
+                    GetDoctorService.save({serviceType:"currentService"}, function (data) {
+                        $scope.curServiceList = data.ListInfo;
+                        console.log("curService",data.ListInfo);
+                    });
+                    GetDoctorService.save({serviceType:"allService"}, function (data) {
+                        $scope.allServiceList = data.ListInfo;
+                        console.log("allService",data.ListInfo);
+                    });
 
+                }
+            });
         };
 
     }]);
