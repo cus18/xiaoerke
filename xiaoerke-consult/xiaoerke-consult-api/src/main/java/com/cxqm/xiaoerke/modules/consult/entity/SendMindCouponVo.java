29f4f303fb77094e2392b531e1f9ccb0c7a045fa
孙晓 <897909806@qq.com>
2016-11-02 16:38:47 +0800

    送心意优惠券

---
+++ xiaoerke-consult/xiaoerke-consult-api/src/main/java/com/cxqm/xiaoerke/modules/consult/entity/SendMindCouponVo.java
@@ -0,0 +1,39 @@
+package com.cxqm.xiaoerke.modules.consult.entity;
+
+
+/**
+ * 送心意优惠卡vo
+ * Created by sunxiao on 2016-10-31.
+ */
+public class SendMindCouponVo {
+
+        private Integer id ;
+
+        private String name;
+
+        private String link ;
+
+        public Integer getId() {
+                return id;
+        }
+
+        public void setId(Integer id) {
+                this.id = id;
+        }
+
+        public String getName() {
+                return name;
+        }
+
+        public void setName(String name) {
+                this.name = name;
+        }
+
+        public String getLink() {
+                return link;
+        }
+
+        public void setLink(String link) {
+                this.link = link;
+        }
+}
