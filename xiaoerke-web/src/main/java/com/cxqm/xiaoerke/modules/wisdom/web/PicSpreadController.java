d54e55691ddfec68ecb3cff3379c52b9b45c8fb0
王保伟 <565871712@qq.com>
2016-11-14 19:00:55 +0800

    结束会话发送消息

---
+++ xiaoerke-web/src/main/java/com/cxqm/xiaoerke/modules/wisdom/web/PicSpreadController.java
@@ -0,0 +1,53 @@
+package com.cxqm.xiaoerke.modules.wisdom.web;
+
+import com.cxqm.xiaoerke.common.utils.ImgUtils;
+import com.cxqm.xiaoerke.modules.consult.entity.OperationPromotionTemplateVo;
+import com.cxqm.xiaoerke.modules.consult.service.OperationPromotionService;
+import com.cxqm.xiaoerke.modules.consult.service.OperationPromotionTemplateService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.jsp.tagext.TagExtraInfo;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+/**
+ * Created by wangbaowei on 16/11/13.
+ */
+
+@Controller
+public class PicSpreadController {
+    @Autowired
+    private OperationPromotionTemplateService operationPromotionTemplateService;
+
+    @RequestMapping(value = "/picSpread/creaet", method = {RequestMethod.POST, RequestMethod.GET})
+    public
+    @ResponseBody
+    Map<String,Object> picSpreadCreat(@RequestParam String  babyName) {
+        Map<String,Object> resultMap = new HashMap<String, Object>();
+        //随机找一个样板,将名字嵌入
+        OperationPromotionTemplateVo templateVo = new OperationPromotionTemplateVo();
+        templateVo.setType("pictureTransmission");
+        List<OperationPromotionTemplateVo> spreadList= operationPromotionTemplateService.findOperationPromotionTemplateList(templateVo);
+        Integer ri = (int)(Math.random()*spreadList.size());
+        String picUrl = "http://xiaoerke-article-pic.oss-cn-beijing.aliyuncs.com/pictureTransmission";
+        String picImg = "";
+        for(int i =0;i<spreadList.size();i++){
+            if(i == ri){
+                picUrl += spreadList.get(i).getId();
+                picImg = ImgUtils.createStringMark(babyName,picUrl+".png",spreadList.get(i).getInfo1()+(spreadList.get(i).getInfo2() == null ?"":";"+spreadList.get(i).getInfo2()));
+                break;
+            }
+        }
+        resultMap.put("picImg",picImg);
+        return  resultMap;
+    }
+}
