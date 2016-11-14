d314e4c9f58bf6e7ebbb957a0edc527b08c6fb4a
王保伟 <565871712@qq.com>
2016-11-14 19:00:15 +0800

    结束会话发送消息

--- wisdom/src/main/webapp/js/controllers/activity/picSpread/picSpreadIndexCtrl.js
+++ wisdom/src/main/webapp/js/controllers/activity/picSpread/picSpreadIndexCtrl.js
@@ -3,9 +3,29 @@ angular.module('controllers', ['ionic']).controller('picSpreadIndexCtrl', [
     function ($scope,$state,$stateParams) {
 
         $scope.title ="图片传播";
+        $scope.info = {};
+        var recordLogs = function(val){
+            $.ajax({
+                url:"util/recordLogs",// 跳转到 action
+                async:true,
+                type:'get',
+                data:{logContent:encodeURI(val)},
+                cache:false,
+                dataType:'json',
+                success:function(data) {
+                },
+                error : function() {
+                }
+            });
+        };
+        recordLogs('TPCB_HDY');
 
         $scope.makePic = function(){
-            $state.go("picSpreadResult");
+
+            if($scope.info.name == ""){
+                alert("请输入宝宝姓名");
+            }
+            $state.go("picSpreadResult",{"name":$scope.info.name});
         };
 
         $scope.$on('$ionicView.enter', function() {
--- wisdom/src/main/webapp/js/controllers/activity/picSpread/picSpreadResultCtrl.js
+++ wisdom/src/main/webapp/js/controllers/activity/picSpread/picSpreadResultCtrl.js
@@ -1,11 +1,41 @@
 angular.module('controllers', ['ionic']).controller('picSpreadResultCtrl', [
-    '$scope','$state','$stateParams',
-    function ($scope,$state,$stateParams) {
+    '$scope','$state','$stateParams','PicSpread',
+    function ($scope,$state,$stateParams,PicSpread) {
         $scope.title ="图片传播";
         $scope.pageLoading =true;// 页面加载是否完成
+        $scope.picUrl = "";
 
+        var recordLogs = function(val){
+            $.ajax({
+                url:"util/recordLogs",// 跳转到 action
+                async:true,
+                type:'get',
+                data:{logContent:encodeURI(val)},
+                cache:false,
+                dataType:'json',
+                success:function(data) {
+                },
+                error : function() {
+                }
+            });
+        };
+        recordLogs('TPCN_SHTP');
+
+       //后台请求图片地址
+        PicSpread.get({babyName:$stateParams.name},function (data) {
+            console.log(data)
+            $scope.picUrl = data.picImg;
+            $scope.pageLoading =false;// 页面加载是否完成
+        });
         $scope.tryAgain = function(){
-            $state.go("picSpreadIndex");
+            //后台请求图片地址
+            recordLogs('TPCN_ZSYC');
+            $scope.pageLoading =true;// 页面加载是否完成
+            PicSpread.get({babyName:$stateParams.name},function (data) {
+                console.log(data)
+                $scope.picUrl = data.picImg;
+                $scope.pageLoading =false;// 页面加载是否完成
+            });
         };
 
         $scope.$on('$ionicView.enter', function() {
