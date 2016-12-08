48a798485305bef10f81094e48aad220b96f11f0
王保伟 <565871712@qq.com>
2016-12-08 15:58:04 +0800

    咨询大夫 文案修改

---
+++ xiaoerke-consult/xiaoerke-consult-api/src/main/java/com/cxqm/xiaoerke/modules/consult/entity/memberRedisCachVo.java
@@ -0,0 +1,13 @@
+package com.cxqm.xiaoerke.modules.consult.entity;
+
+/**
+ * Created by wangbaowei on 16/12/8.
+ */
+public enum  memberRedisCachVo {
+    NIKE_NAME,
+    OPENID,
+    HEAD_URL,
+    MARKET,
+    ATTENTION_DATE,
+    MEMBER_END_DATE
+}
