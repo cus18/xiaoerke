b40c94b243cbef0b91c7cec99dd13158fc4fd3ea
王保伟 <565871712@qq.com>
2016-12-09 15:48:14 +0800

    咨询大夫 文案修改

--- xiaoerke-wechat/xiaoerke-wechat-service/src/main/java/com/cxqm/xiaoerke/modules/wechat/service/impl/WechatPatientCoreServiceImpl.java
+++ xiaoerke-wechat/xiaoerke-wechat-service/src/main/java/com/cxqm/xiaoerke/modules/wechat/service/impl/WechatPatientCoreServiceImpl.java
@@ -1708,6 +1708,7 @@ public class WechatPatientCoreServiceImpl implements WechatPatientCoreService {
         //检测当前用户会员是否过期(没有会员按未过期处理)
         String memberEndTime = consultMemberRedsiCacheService.getConsultMember(openid+memberRedisCachVo.MEMBER_END_DATE);
         if(null == memberEndTime||DateUtils.StrToDate(memberEndTime,"datetime").getTime()<nowDate.getTime()){
+            SysPropertyVoWithBLOBsVo sysPropertyVoWithBLOBsVo = sysPropertyService.querySysProperty();
 //            说明是新用户或者是用户的会员已过期,要检测是否是今日 首次咨询以及是否有机会
                 String datetime = DateUtils.DateToStr(nowDate,"date");
                 String latestConsultTime = consultMemberRedsiCacheService.getConsultMember(openid+memberRedisCachVo.LATEST_CONSULT_TIME);
@@ -1721,14 +1722,14 @@ public class WechatPatientCoreServiceImpl implements WechatPatientCoreService {
                     }else{
                         //没有机会,推送购买链接
                         String content = "求助客服请直接向分诊说明，不需付费\n<a href='http://www.baodi.com'>h5页面的入口</a>" +
-                                "\n\n您好，由于本月的咨询机会已经用完，为了给你更好更快的服务，需要购买咨询服务\n-----------\n<a href='http://www.baodi.com'>>>点击这里购买更多咨询服务</a>";
+                                "\n\n您好，由于本月的咨询机会已经用完，为了给你更好更快的服务，需要购买咨询服务\n-----------\n<a href='"+sysPropertyVoWithBLOBsVo.getKeeperWebUrl()+"/keeper/wechatInfo/fieldwork/wechat/author?url="+sysPropertyVoWithBLOBsVo.getKeeperWebUrl()+"/keeper/wechatInfo/getUserWechatMenId?url=35'>点击这里购买更多咨询服务</a>";
                         WechatUtil.sendMsgToWechat(token,openid,content);
                         return  false;
                     }
                 }
             //会员时间超时,推送购买链接
             String content = "求助客服请直接向分诊说明，不需付费\n<a href='http://www.baodi.com'>h5页面的入口</a>" +
-                    "\n\n您好，由于本月的咨询机会已经用完，为了给你更好更快的服务，需要购买咨询服务\n-----------\n<a href='http://www.baodi.com'>>>点击这里购买更多咨询服务</a>";
+                    "\n\n您好，由于本月的咨询机会已经用完，为了给你更好更快的服务，需要购买咨询服务\n-----------\n<a href='"+sysPropertyVoWithBLOBsVo.getKeeperWebUrl()+"/keeper/wechatInfo/fieldwork/wechat/author?url="+sysPropertyVoWithBLOBsVo.getKeeperWebUrl()+"/keeper/wechatInfo/getUserWechatMenId?url=35'>点击这里购买更多咨询服务</a>";
             WechatUtil.sendMsgToWechat(token,openid,content);
             return false;
         }
