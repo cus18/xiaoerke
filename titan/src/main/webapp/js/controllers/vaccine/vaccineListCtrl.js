ac3d287708edd9251d7d225f3ca2608ed9586bdd
lipangpangz <270255061>
2016-12-12 18:36:09 +0800

    babyCoin

--- titan/src/main/webapp/js/controllers/vaccine/vaccineListCtrl.js
+++ titan/src/main/webapp/js/controllers/vaccine/vaccineListCtrl.js
@@ -2,6 +2,27 @@
 angular.module('controllers', ['ionic']).controller('vaccineListCtrl', [
     '$scope',
     function ($scope) {
+        $scope.info = {
+            shoeFree:false,
+            showCharge:true
+        };
+        $scope.vaccineList = [
+            {
+                vaccine:'乙肝疫苗',
+                age:'月龄',
+                num:'第1/3针'
+            },
+            {
+                vaccine:'甲肝疫苗',
+                age:'月龄',
+                num:'第1/3针'
+            },
+            {
+                vaccine:'乙肝疫苗',
+                age:'月龄',
+                num:'第1/3针'
+            }
+        ];
 
     }]);
 
