132be9275d6269f0fac429993b41ce18b01f1c4f
王保伟 <565871712@qq.com>
2016-12-12 17:30:40 +0800

    咨询大夫 文案修改

--- xiaoerke-web/src/main/java/com/cxqm/xiaoerke/modules/consult/web/ConsultDoctorTransferController.java
+++ xiaoerke-web/src/main/java/com/cxqm/xiaoerke/modules/consult/web/ConsultDoctorTransferController.java
@@ -217,10 +217,10 @@ public class ConsultDoctorTransferController extends BaseController {
                         String token = (String) parameter.get("token");
                         ConsultSession consultSession =consultSessionService.selectByPrimaryKey(consultSessionForwardRecordsVo.getConversationId().intValue());
                         //根据接入的是否为医生来判断
-                        if("consultDoctor".equals(user.getUserType())&&consultMemberRedsiCacheService.consultChargingCheck(consultSession.getCsUserId(),token)){
+                        if("consultDoctor".equals(user.getUserType())&&consultMemberRedsiCacheService.consultChargingCheck(consultSession.getUserId(),token)){
 //                        增加机会
                             SysPropertyVoWithBLOBsVo sysPropertyVoWithBLOBsVo = sysPropertyService.querySysProperty();
-                            consultMemberRedsiCacheService.useFreeChance(consultSession.getCsUserId(),sysPropertyVoWithBLOBsVo.getFreeConsultMemberTime());
+                            consultMemberRedsiCacheService.useFreeChance(consultSession.getUserId(),sysPropertyVoWithBLOBsVo.getFreeConsultMemberTime());
                         };
                     }catch (Exception e){
                         e.printStackTrace();
