d89deac52e29df656d4732548d0f61b74cd49672
jiangzhongge <jzg_lucky@163.com>
2016-12-14 12:43:30 +0800

    jiangzg

--- titan/src/main/webapp/js/controllers/vaccine/vaccineNotifyCtrl.js
+++ titan/src/main/webapp/js/controllers/vaccine/vaccineNotifyCtrl.js
@@ -1,12 +1,13 @@
 
 angular.module('controllers', ['ionic']).controller('vaccineNotifyCtrl', [
-    '$scope','GetOneVaccineInfoList','$stateParams',
-    function ($scope,GetOneVaccineInfoList,$stateParams) {
+    '$scope','GetOneVaccineInfoList','$stateParams','$sce',
+    function ($scope,GetOneVaccineInfoList,$stateParams,$sce) {
         $scope.doRefresh = function () {
             GetOneVaccineInfoList.save({id:$stateParams.id},function(data){
                 if(data.status == "success"){
-                    $scope.informedForm = data.dataList[0].informedForm;
-                    console.log($scope.informedForm);
+                    $scope.informedForm = $sce.trustAsHtml(data.dataList[0].informedForm);
+                    $scope.informedFormTitle = data.dataList[0].informedFormTitle;
+                    console.log(data.dataList[0].informedForm);
                 }
             });
         };
