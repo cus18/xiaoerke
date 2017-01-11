e9dc724c6fe535f7fde606411c64a7144d3e683d
zhaodeliang <1975063953@qq.com>
2017-01-11 12:11:07 +0800

    New changelist

--- xiaoerke-web/src/main/java/com/cxqm/xiaoerke/modules/consult/web/ConsultUserController.java
+++ xiaoerke-web/src/main/java/com/cxqm/xiaoerke/modules/consult/web/ConsultUserController.java
@@ -191,10 +191,10 @@ public class ConsultUserController extends BaseController {
             if (csUserId.equals("allCS")) {
                 query = new Query().with(new Sort(Sort.Direction.DESC, "lastMessageTime"));
             } else {
-                query = new Query().addCriteria(new Criteria().where("csUserId").regex(csUserId)).with(new Sort(Sort.Direction.DESC, "lastMessageTime"));
+                query = new Query().addCriteria(new Criteria().where("csUserId").is(csUserId)).with(new Sort(Sort.Direction.DESC, "lastMessageTime"));
             }
         } else if (!csUserId.equals("allCS")) {
-            query = new Query().addCriteria(new Criteria().where("csUserId").regex(csUserId).andOperator(Criteria.where("lastMessageTime").gte(date))).with(new Sort(Sort.Direction.DESC, "lastMessageTime"));
+            query = new Query().addCriteria(new Criteria().where("csUserId").is(csUserId).andOperator(Criteria.where("lastMessageTime").gte(date))).with(new Sort(Sort.Direction.DESC, "lastMessageTime"));
         } else {
             query = new Query().addCriteria(Criteria.where("lastMessageTime").gte(date)).with(new Sort(Sort.Direction.DESC, "lastMessageTime"));
             ;
