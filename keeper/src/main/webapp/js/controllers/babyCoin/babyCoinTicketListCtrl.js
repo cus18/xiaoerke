3ecfd8c8ab41ec73675f74b248abcb935b606f4d
izhengly <820359064@qq.com>
2017-02-09 18:46:28 +0800

    babyCoin

--- keeper/src/main/webapp/js/controllers/babyCoin/babyCoinTicketListCtrl.js
+++ keeper/src/main/webapp/js/controllers/babyCoin/babyCoinTicketListCtrl.js
@@ -3,7 +3,42 @@ angular.module('controllers', ['ionic']).controller('babyCoinTicketListCtrl', [
     function ($scope,$state,$stateParams,sendMindCouponList) {
 
         $scope.commitLock=true;
-
+        $scope.ticketData=[
+            {
+                name:"咨询5折券",
+                coin:"50",
+                pic:"http://xiaoerke-wxapp-pic.oss-cn-hangzhou.aliyuncs.com/babyCoin/ticket5_1.png"
+            },
+            {
+                name:"5元代金券",
+                coin:"50",
+                pic:"http://xiaoerke-wxapp-pic.oss-cn-hangzhou.aliyuncs.com/babyCoin/ticket5_2.png"
+            },
+            {
+                name:"10元代金券",
+                coin:"100",
+                pic:"http://xiaoerke-wxapp-pic.oss-cn-hangzhou.aliyuncs.com/babyCoin/ticket10.png"
+            },
+            {
+                name:"20元代金券",
+                coin:"200",
+                pic:"http://xiaoerke-wxapp-pic.oss-cn-hangzhou.aliyuncs.com/babyCoin/ticket20.png"
+            },
+            {
+                name:"30元代金券",
+                coin:"300",
+                pic:"http://xiaoerke-wxapp-pic.oss-cn-hangzhou.aliyuncs.com/babyCoin/ticket30.png"
+            },
+            {
+                name:"40元代金券",
+                coin:"400",
+                pic:"http://xiaoerke-wxapp-pic.oss-cn-hangzhou.aliyuncs.com/babyCoin/ticket40.png"
+            }
+        ];
+        //点击 赚取宝宝币
+        $scope.earnBabyCoin = function () {
+            $state.go("babyCoinInvitePage");
+        };
         sendMindCouponList.save({},function (data) {
             $scope.couponList=data.mindCoupon;
         })
