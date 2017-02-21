3b1475fded4279f262149cbcc502f12f533c8590
zhaodeliang <1975063953@qq.com>
2017-02-21 10:49:29 +0800

    formartCOde

--- keeper/src/main/webapp/js/pay/doctorConsultPay.js
+++ keeper/src/main/webapp/js/pay/doctorConsultPay.js
@@ -214,31 +214,35 @@ function wechatPay() {
                 },
                 cache: false,
                 success: function (data) {
-                    $('#payButton').removeAttr("disabled");
-                    var obj = eval('(' + data + ')');
-                    if (parseInt(obj.agent) < 5) {
-                        alert("您的微信版本低于5.0无法使用微信支付");
-                        return;
-                    }
-                    //打开微信支付控件
-                    wx.chooseWXPay({
-                        appId: obj.appId,
-                        timestamp: obj.timeStamp, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
-                        nonceStr: obj.nonceStr,  // 支付签名随机串，不长于 32 位
-                        package: obj.package,// 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=***）
-                        signType: obj.signType, // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
-                        paySign: obj.paySign,  // 支付签名
-                        success: function (res) {
-                            if (res.errMsg == "chooseWXPay:ok") {
-                                window.location.href = "http://s132.baodf.com/angel/patient/consult#/doctorConsultPaySuccess";
-                            } else {
-                                alert("支付失败,请重新支付")
-                            }
-                        },
-                        fail: function (res) {
-                            alert(res.errMsg)
+                    if(data == "false"){
+                        alertD("对不起，你的操作异常！请正确操作！");
+                    }else{
+                        $('#payButton').removeAttr("disabled");
+                        var obj = eval('(' + data + ')');
+                        if (parseInt(obj.agent) < 5) {
+                            alert("您的微信版本低于5.0无法使用微信支付");
+                            return;
                         }
-                    });
+                        //打开微信支付控件
+                        wx.chooseWXPay({
+                            appId: obj.appId,
+                            timestamp: obj.timeStamp, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
+                            nonceStr: obj.nonceStr,  // 支付签名随机串，不长于 32 位
+                            package: obj.package,// 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=***）
+                            signType: obj.signType, // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
+                            paySign: obj.paySign,  // 支付签名
+                            success: function (res) {
+                                if (res.errMsg == "chooseWXPay:ok") {
+                                    window.location.href = "http://s132.baodf.com/angel/patient/consult#/doctorConsultPaySuccess";
+                                } else {
+                                    alert("支付失败,请重新支付")
+                                }
+                            },
+                            fail: function (res) {
+                                alert(res.errMsg)
+                            }
+                        });
+                    }
                 },
                 error: function () {
                 }
