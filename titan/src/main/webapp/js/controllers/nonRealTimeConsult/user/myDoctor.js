9ebefa4665ab83133e412cfdcff9a8d37be6d647
王保伟 <565871712@qq.com>
2017-02-22 14:16:24 +0800

    window.location.href=

--- titan/src/main/webapp/js/controllers/nonRealTimeConsult/user/myDoctor.js
+++ titan/src/main/webapp/js/controllers/nonRealTimeConsult/user/myDoctor.js
@@ -1,5 +1,17 @@
 ﻿angular.module('controllers', []).controller('myDoctor', [
-    '$scope','$stateParams',
-    function ($scope,$stateParams) {
+    '$scope','$stateParams','recentTimeList',
+    function ($scope,$stateParams,recentTimeList) {
         $scope.title='我的医生';
+        $scope.doctorInfo = {};
+
+        // 我咨询记录的数据
+        recentTimeList.save({},function (data) {
+            if("success" == data.status){
+                $scope.doctorInfo = data.departmentVoList;
+                console.log(data)
+            }else{
+                alert("请重新打开页面");
+            }
+
+        })
     }]);
