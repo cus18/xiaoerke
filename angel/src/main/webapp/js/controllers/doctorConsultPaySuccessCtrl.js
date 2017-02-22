cd4858c403571a8c4f2dab4e07863e6d7cb1f057
zhaodeliang <1975063953@qq.com>
2017-02-22 16:34:36 +0800

    formartCOde

--- angel/src/main/webapp/js/controllers/doctorConsultPaySuccessCtrl.js
+++ angel/src/main/webapp/js/controllers/doctorConsultPaySuccessCtrl.js
@@ -1,10 +1,23 @@
 angular.module('controllers', ['luegg.directives','ngFileUpload'])
     .controller('doctorConsultPaySuccessCtrl', ['$scope',
         function ($scope) {
+            $scope.consultTime = "";
             $scope.doctorConsultPaySuccess = function () {
+                $scope.consultTime = getQueryString("consultTime");
+                if($scope.consultTime == "30")
+                    $scope.consultTime = "30分钟";
+                else
+                    $scope.consultTime = "24小时";
+
             };
             $scope.closeDoctorConsultPaySuccess = function () {
                 wx.closeWindow();
             };
+
+            function getQueryString(name) {
+                var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+                var r = window.location.search.substr(1).match(reg);
+                if (r != null) return unescape(r[2]); return null;
+            }
         }])
 
