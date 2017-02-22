13fca22ba0c6caf31af5522297a36a63c097338e
Administrator <270255061@qq.com>
2017-02-22 12:13:01 +0800

    pay

---
+++ titan/src/main/webapp/js/controllers/phoneConsult/myDoctor.js
@@ -0,0 +1,7 @@
+﻿angular.module('controllers', ['ionic']).controller('myDoctor', [
+        '$scope','$state','MyselfInfo','$ionicPopup','GetUserLoginStatus',
+    '$rootScope','$location','resolveUserLoginStatus','$stateParams','RecordLogs','BabyCoinInit',
+        function ($scope,$state,MyselfInfo,$ionicPopup,GetUserLoginStatus,$rootScope,$location,resolveUserLoginStatus,$stateParams,RecordLogs,BabyCoinInit) {
+            $scope.title='我的医生';
+            $scope.name='我是医生列表界面';
+    }])
