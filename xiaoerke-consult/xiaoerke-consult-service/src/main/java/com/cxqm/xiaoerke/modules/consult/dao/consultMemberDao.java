6fdf0846f790b6a7f8c25e209e9d7fbb3885bfe6
cjk18 <cus18@sina.com>
2017-02-22 14:52:42 +0800

    Merge branch 'master' of E:\xiaoerke12-31\xiaoerke with conflicts.

--- xiaoerke-consult/xiaoerke-consult-service/src/main/java/com/cxqm/xiaoerke/modules/consult/dao/consultMemberDao.java
+++ xiaoerke-consult/xiaoerke-consult-service/src/main/java/com/cxqm/xiaoerke/modules/consult/dao/consultMemberDao.java
@@ -5,8 +5,6 @@ import com.cxqm.xiaoerke.common.persistence.Page;
 import com.cxqm.xiaoerke.common.persistence.annotation.MyBatisDao;
 import com.cxqm.xiaoerke.modules.consult.entity.ConsultMemberVo;
 
-import java.util.List;
-
 @MyBatisDao
 public interface ConsultMemberDao {
     int deleteByPrimaryKey(Integer id);
@@ -24,8 +22,4 @@ public interface ConsultMemberDao {
     int updateByPrimaryKey(ConsultMemberVo record);
 
     Page<ConsultMemberVo> findConsultMemberList(ConsultMemberVo vo,Page<ConsultMemberVo> page);
-
-    List<ConsultMemberVo> getConsultMemberList(ConsultMemberVo vo);
-
-
 }
