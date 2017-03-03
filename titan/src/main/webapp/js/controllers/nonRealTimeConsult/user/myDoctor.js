334265d0cd24a37459ea5edcb0a3a3830eabe715
王保伟 <565871712@qq.com>
2017-03-03 16:38:05 +0800

    window.location.href=

--- titan/src/main/webapp/js/controllers/nonRealTimeConsult/user/myDoctor.js
+++ titan/src/main/webapp/js/controllers/nonRealTimeConsult/user/myDoctor.js
@@ -4,16 +4,18 @@
         $scope.title='我的医生';
         $scope.doctorInfo = {};
 
-        // 我咨询记录的数据
-        recentTimeList.save({},function (data) {
-            if("success" == data.status){
-                $scope.doctorInfo = data.departmentVoList;
-                console.log(data)
-            }else{
-                alert("请重新打开页面");
-            }
+        $scope.$on('$ionicView.enter',function() {
+            // 我咨询记录的数据
+            recentTimeList.save({},function (data) {
+                if("success" == data.status){
+                    $scope.doctorInfo = data.departmentVoList;
+                    console.log(data)
+                }else{
+                    alert("请重新打开页面");
+                }
 
-        })
+            })
+        });
 
         $scope.checkDoctorInformation = function(index){
             console.log($scope.doctorInfo[index].doctorId);
