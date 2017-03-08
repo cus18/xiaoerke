333f46715a360cbc84a680f8dc16231f212ecf42
Administrator <270255061@qq.com>
2017-03-08 16:58:29 +0800

    Rejected commit: Default

--- titan/src/main/webapp/js/controllers/appHusband/husResultCtrl.js
+++ titan/src/main/webapp/js/controllers/appHusband/husResultCtrl.js
@@ -2,8 +2,8 @@ angular.module('controllers', ['ionic','ngDialog']).controller('husResultCtrl', 
     '$scope','$state','$stateParams','MyselfInfoAppointmentDetail',
     'OrderPayMemberServiceOperation','GetUserMemberService','$location','ngDialog',
     function ($scope,$state,$stateParams,MyselfInfoAppointmentDetail,OrderPayMemberServiceOperation,GetUserMemberService,$location,ngDialog) {
-        $scope.status1=false;
+        $scope.status1=true;
         $scope.status2=false;
         $scope.status3=false;
-        $scope.status4=true;
+        $scope.status4=false;
     }])
