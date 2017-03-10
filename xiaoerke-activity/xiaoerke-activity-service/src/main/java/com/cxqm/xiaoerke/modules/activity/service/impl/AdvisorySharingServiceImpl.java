cbe11c943dfde78515dd707aa822373c9b59eab9
王保伟 <565871712@qq.com>
2017-03-10 11:35:46 +0800

    window.location.href=

--- xiaoerke-activity/xiaoerke-activity-service/src/main/java/com/cxqm/xiaoerke/modules/activity/service/impl/AdvisorySharingServiceImpl.java
+++ xiaoerke-activity/xiaoerke-activity-service/src/main/java/com/cxqm/xiaoerke/modules/activity/service/impl/AdvisorySharingServiceImpl.java
@@ -1,10 +1,16 @@
 package com.cxqm.xiaoerke.modules.activity.service.impl;
 
 import com.cxqm.xiaoerke.modules.activity.service.AdvisorySharingService;
+import com.cxqm.xiaoerke.modules.consult.entity.ConsultDoctorInfoVo;
 import com.cxqm.xiaoerke.modules.consult.entity.ConsultRecordMongoVo;
+import com.cxqm.xiaoerke.modules.consult.entity.ConsultSession;
 import com.cxqm.xiaoerke.modules.consult.entity.ConsultSessionStatusVo;
+import com.cxqm.xiaoerke.modules.consult.service.ConsultDoctorInfoService;
 import com.cxqm.xiaoerke.modules.consult.service.ConsultRecordService;
+import com.cxqm.xiaoerke.modules.consult.service.ConsultSessionService;
 import com.cxqm.xiaoerke.modules.sys.entity.PaginationVo;
+import com.cxqm.xiaoerke.modules.wechat.entity.WechatAttention;
+import com.cxqm.xiaoerke.modules.wechat.service.WechatAttentionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.query.Criteria;
@@ -32,13 +38,31 @@ public class AdvisorySharingServiceImpl implements AdvisorySharingService{
     @Autowired
     private ConsultRecordService consultRecordService;
 
+    @Autowired
+    private ConsultDoctorInfoService consultDoctorInfoService;
+
+    @Autowired
+    private ConsultSessionService consultSessionService;
+
+    @Autowired
+    private WechatAttentionService wechatAttentionService;
+
     @Override
     public Map<String ,Object> conversationRecord(String sessionid,int pageNo,int pageSize) {
         Map<String ,Object> resultMap = new HashMap<String, Object>();
-        Query query = new Query(where("sessionid").is(sessionid)).with(new Sort(Sort.Direction.ASC, "createDate"));
+
+        //聊天记录
+        Query query = new Query(where("sessionId").is(sessionid)).with(new Sort(Sort.Direction.ASC, "createDate"));
         PaginationVo<ConsultRecordMongoVo> pagination = consultRecordService.getRecordDetailInfo(pageNo, pageSize, query, "permanent");
         List<ConsultRecordMongoVo> recordList  = pagination.getDatas();
         resultMap.put("recordList",recordList);
+        //医生信息
+        ConsultSession consultSession = consultSessionService.selectByPrimaryKey(Integer.parseInt(sessionid));
+        ConsultDoctorInfoVo doctorInfoVo = consultDoctorInfoService.getConsultDoctorInfoByUserId(consultSession.getCsUserId());
+        resultMap.put("doctorInfoVo",doctorInfoVo);
+        //患者信息
+        WechatAttention attentionInfo = wechatAttentionService.getAttentionByOpenId(consultSession.getUserId());
+        resultMap.put("attentionInfo",attentionInfo);
         return resultMap;
     }
 
