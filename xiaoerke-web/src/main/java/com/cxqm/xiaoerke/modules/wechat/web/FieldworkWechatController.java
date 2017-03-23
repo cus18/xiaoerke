291023de277c7b9b39e92b0321e42b6308241563
王保伟 <565871712@qq.com>
2017-03-23 13:08:10 +0800

    window.location.href=

--- xiaoerke-web/src/main/java/com/cxqm/xiaoerke/modules/wechat/web/FieldworkWechatController.java
+++ xiaoerke-web/src/main/java/com/cxqm/xiaoerke/modules/wechat/web/FieldworkWechatController.java
@@ -246,7 +246,7 @@ public class FieldworkWechatController {
         }else if(url.startsWith("54")){  //集卡活动
             url = sysPropertyVoWithBLOBsVo.getTitanWebUrl() + "titan/appHusband#/husGuide";
         }else if(url.startsWith("57")){  //打卡活动
-            url = sysPropertyVoWithBLOBsVo.getTitanWebUrl() + "titan/sign#signHome";
+            url = sysPropertyVoWithBLOBsVo.getTitanWebUrl() + "titan/sign#signHome/false";
         }
         String get_access_token_url = "https://api.weixin.qq.com/sns/oauth2/access_token?" +
                 "appid=APPID" +
