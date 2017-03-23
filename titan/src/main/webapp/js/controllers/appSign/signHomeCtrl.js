afd4964375dd62b0104a7174e0021b5483757994
Administrator <270255061@qq.com>
2017-03-23 13:07:15 +0800

    Rejected commit: Default

--- titan/src/main/webapp/js/controllers/appSign/signHomeCtrl.js
+++ titan/src/main/webapp/js/controllers/appSign/signHomeCtrl.js
@@ -77,6 +77,7 @@ angular.module('controllers', ['ionic','ngDialog']).controller('signHomeCtrl', [
             }else{
                 $scope.oData=res;
                 $scope.openId=res.openId;
+                $scope.market=res.market;
                 if(res.isOrNotPay=='no'){
                     $scope.goJoinStatus=true;
                     $scope.goSignStatus=false;
@@ -87,90 +88,77 @@ angular.module('controllers', ['ionic','ngDialog']).controller('signHomeCtrl', [
             }
         })
         //初始化微信
-        var doRefresh = function () {
-            var timestamp;//时间戳
-            var nonceStr;//随机字符串
-            var signature;//得到的签名
-            var appid;//得到的签名
-            $.ajax({
-                url: "wechatInfo/getConfig",// 跳转到 action
-                async: true,
-                type: 'get',
-                data: {url: location.href},//得到需要分享页面的url
-                cache: false,
-                dataType: 'json',
-                success: function (data){
-                    if (data != null){
-                        timestamp = data.timestamp;//得到时间戳
-                        nonceStr = data.nonceStr;//得到随机字符串
-                        signature = data.signature;//得到签名
-                        appid = data.appid;//appid
-                        //微信配置
-                        wx.config({
-                            debug: false,
-                            appId: appid,
-                            timestamp: timestamp,
-                            nonceStr: nonceStr,
-                            signature: signature,
-                            jsApiList: [
-                                'onMenuShareTimeline',
-                                'onMenuShareAppMessage',
-                                'chooseWXPay'
-                            ] // 功能列表
-                        });
-                        wx.ready(function () {
-                            wx.hideOptionMenu();
-                        });
-                    } else {
-                    }
-                },
-                error: function () {
-                    
-                }
-            });
-        };
+        var doRefresh = function ($scope) {
+            GetConfig.save({}, function (data) {
+                $scope.inviteUrlData = data.publicSystemInfo.redPackageShareUrl;
+                var share = $scope.inviteUrlData + $scope.openid + "," + $scope.market + ",";//最后url=41，openid,marketer
 
+                // var share = $scope.inviteUrlData + $scope.openid+","+$scope.marketer+","+ $scope.uuid+",";//最后url=41，openid,marketer
+                // if(version=="a"){
+                version = "a";
+                var timestamp;//时间戳
+                var nonceStr;//随机字符串
+                var signature;//得到的签名
+                var appid;//得到的签名
+                $.ajax({
+                    url: "wechatInfo/getConfig",// 跳转到 action
+                    async: true,
+                    type: 'get',
+                    data: {url: location.href.split('#')[0]},//得到需要分享页面的url
+                    cache: false,
+                    dataType: 'json',
+                    success: function (data) {
+                        if (data != null) {
+                            timestamp = data.timestamp;//得到时间戳
+                            nonceStr = data.nonceStr;//得到随机字符串
+                            signature = data.signature;//得到签名
+                            appid = data.appid;//appid
+                            //微信配置
+                            wx.config({
+                                debug: false,
+                                appId: appid,
+                                timestamp: timestamp,
+                                nonceStr: nonceStr,
+                                signature: signature,
+                                jsApiList: [
+                                    'onMenuShareTimeline',
+                                    'onMenuShareAppMessage',
+                                    'previewImage'
+                                ] // 功能列表
+                            });
+                            wx.ready(function () {
+                                // 2.2 监听“分享到朋友圈”按钮点击、自定义分享内容及分享结果接口
+                                wx.onMenuShareTimeline({
+                                    title: '感恩妈妈节，在这里可以免费咨询三甲医院儿科专家,还有机会赢现金大礼', // 分享标题
+                                    link: share, // 分享链接
+                                    imgUrl: 'http://xiaoerke-pc-baodf-pic.oss-cn-beijing.aliyuncs.com/invite/patientConsultInvitePage.jpg', // 分享图标
+                                    success: function (res) {
+                                        //recordLogs("ZXYQ_YQY_SHARE");
+                                        // redPacketCreate.save({"uuid":$scope.uuid},function (data) {
+                                        // });
+                                    },
+                                    fail: function (res) {
 
-
-        //确认支付
-        function wechatPay() {
-            recordLogs("consult_charge_twice_paypage_paybutton");
-            $.ajax({
-                url: "account/user/doctorConsultPay",
-                type: 'get',
-                data: {
-                    payPrice:endCoin ,
-                    babyCoinNumber: cashNum*10
-                },
-                cache: false,
-                success: function (data) {
-                    var obj = eval('(' + data + ')');
-                    if (parseInt(obj.agent) < 5) {
-                        alert("您的微信版本低于5.0无法使用微信支付");
-                        return;
-                    }
-                    //打开微信支付控件
-                    wx.chooseWXPay({
-                        appId: obj.appId,
-                        timestamp: obj.timeStamp, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
-                        nonceStr: obj.nonceStr,  // 支付签名随机串，不长于 32 位
-                        package: obj.package,// 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=***）
-                        signType: obj.signType, // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
-                        paySign: obj.paySign,  // 支付签名
-                        success: function (res) {
-                            if (res.errMsg == "chooseWXPay:ok") {
-
-                            } else {
-                                alert("支付失败,请重新支付")
-                            }
-                        },
-                        fail: function (res) {
-                            alert(res.errMsg)
+                                    }
+                                });
+                                wx.onMenuShareAppMessage({
+                                    title: $scope.minename + '向你推荐', // 分享标题
+                                    desc: '感恩妈妈节，在这里可以免费咨询三甲医院儿科专家,还有机会赢现金大礼 ',// 分享描述
+                                    link: share, // 分享链接
+                                    imgUrl: 'http://xiaoerke-pc-baodf-pic.oss-cn-beijing.aliyuncs.com/invite/patientConsultInvitePage.jpg', // 分享图标
+                                    success: function (res) {
+                                    },
+                                    fail: function (res) {
+                                    }
+                                });
+                            })
+                        } else {
                         }
-                    });
-                },
-                error: function () {
-                }
+                    },
+                    error: function () {
+                    }
+                });
+
             });
         }
     }])
