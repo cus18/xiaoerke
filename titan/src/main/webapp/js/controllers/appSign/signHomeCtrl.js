dc3c7e04b5948aa6ee138d7e720dee9b44d5a55a
Administrator <270255061@qq.com>
2017-03-27 17:32:36 +0800

    Rejected commit: Default

--- titan/src/main/webapp/js/controllers/appSign/signHomeCtrl.js
+++ titan/src/main/webapp/js/controllers/appSign/signHomeCtrl.js
@@ -117,13 +117,14 @@ angular.module('controllers', ['ionic','ngDialog']).controller('signHomeCtrl', [
         //查看更多记录
         $scope.lookMore=function(){
             GetPunchCardRewards.save({pageNo:$scope.pageNum++,pageSize:10},function(res){
-                $scope.dataList=$scope.dataList.concat(res.personRewardsList);
-                for(var i=0;i<$scope.dataList.length;i++){
-                    $scope.dataList[i].hour=getHour($scope.dataList[i].updateTime);
-                    if($scope.dataList[i].headImgUrl==''||$scope.dataList[i].headImgUrl==null){
-                        $scope.dataList[i].headImgUrl='http://xiaoerke-pc-baodf-pic.oss-cn-beijing.aliyuncs.com/dkf/consult/yonghumoren.png';
+                var moreList=res.personRewardsList;
+                for(var i=0;i<moreList.length;i++){
+                    moreList[i].hour=getHour(moreList[i].updateTime);
+                    if(moreList[i].headImgUrl==''||moreList[i].headImgUrl==null){
+                        moreList[i].headImgUrl='http://xiaoerke-pc-baodf-pic.oss-cn-beijing.aliyuncs.com/dkf/consult/yonghumoren.png';
                     }
                 }
+                $scope.dataList=$scope.dataList.concat(res.personRewardsList);
             })
         }
 
