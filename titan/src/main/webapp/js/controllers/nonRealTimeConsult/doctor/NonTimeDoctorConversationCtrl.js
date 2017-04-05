51aaa67f9e4d7e10127802a79ea0d7ee49a637df
王保伟 <565871712@qq.com>
2017-04-05 17:33:41 +0800

    window.location.href=

--- titan/src/main/webapp/js/controllers/nonRealTimeConsult/doctor/NonTimeDoctorConversationCtrl.js
+++ titan/src/main/webapp/js/controllers/nonRealTimeConsult/doctor/NonTimeDoctorConversationCtrl.js
@@ -151,7 +151,10 @@ angular.module('controllers', ['ngFileUpload']).controller('NonTimeDoctorConvers
                     }, function (data) {
                         console.log("会话信息列表", data)
                         $scope.pageData = data;
-                        $scope.messageList = data.messageList;
+                        for(var i=0;i<data.messageList.length;i++){
+                            data.messageList[i].message = $sce.trustAsHtml(angular.copy(data.messageList[i].message));
+                        }
+                        $scope.messageList = $scope.pageData.messageList;
                         $scope.doctorId = $scope.pageData.doctorId;
                         $('.pic-talk').each(function(){
                             picList.push($(this).attr('src'));
