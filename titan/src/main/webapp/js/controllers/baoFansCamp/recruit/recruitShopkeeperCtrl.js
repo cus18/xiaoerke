c0efea6f7c5e0c8b1af26d0ac0381c463ca7ca50
izhengly <820359064@qq.com>
2017-04-10 14:25:31 +0800

    recruit

---
+++ titan/src/main/webapp/js/controllers/baoFansCamp/recruit/recruitShopkeeperCtrl.js
@@ -0,0 +1,67 @@
+﻿angular.module('controllers', ['ionic']).controller('recruitShopkeeperCtrl', [
+    '$scope','$state','$stateParams','getUser2016Data',
+    function ($scope,$state,$stateParams,getUser2016Data) {
+        $scope.dataShopkeeper = {};//店长填写的信息
+        $scope.topValue = "";//填写框距离顶部的值
+
+        // 点击 立即申请
+        $scope.commitInfo =function(){
+            console.log("店长填写信息显示", $scope.dataShopkeeper);
+        };
+        // 输入框 获取焦点事件
+        $scope.skipTop =function(){
+            $(".slide61").hide();
+            $(".swiper-pagination").hide();
+           /* $(".fillWrap").css("top","20px");
+            $scope.topValue=$(".fillWrap").css();*/
+        };
+        // 输入框数 失去焦点事件
+        $scope.skipOriginal =function(){
+            $(".slide61").show();
+            $(".swiper-pagination").show();
+           /* $(".fillWrap").css("top",$scope.topValue+"px");*/
+        };
+
+
+        $scope.$on('$ionicView.enter',function() {
+            /*var pageHeight=document.body.clientHeight;*/
+            var pageHeight=$(window).height();
+             $(".swiper-container").css("height",pageHeight+"px");
+            console.log("手机屏幕高度",pageHeight);
+
+            getUser2016Data.save({},function (data) {
+                console.log("2016数据",data);
+
+
+            });
+            var scaleW=window.innerWidth/320;
+            var scaleH=window.innerHeight/480;
+            var resizes = document.querySelectorAll('.resize');
+            for (var j=0; j<resizes.length; j++) {
+                resizes[j].style.width=parseInt(resizes[j].style.width)*scaleW+'px';
+                resizes[j].style.height=parseInt(resizes[j].style.height)*scaleH+'px';
+                resizes[j].style.top=parseInt(resizes[j].style.top)*scaleH+'px';
+                resizes[j].style.bottom=parseInt(resizes[j].style.bottom)*scaleH+'px';
+                resizes[j].style.left=parseInt(resizes[j].style.left)*scaleW+'px';
+                resizes[j].style.right=parseInt(resizes[j].style.right)*scaleW+'px';
+            }
+            var mySwiper = new Swiper ('.swiper-container', {
+                direction : 'horizontal',
+                pagination: '.swiper-pagination',
+                onInit: function(swiper){ //Swiper2.x的初始化是onFirstInit
+                    swiperAnimateCache(swiper); //隐藏动画元素
+                    swiperAnimate(swiper); //初始化完成开始动画
+                },
+                onSlideChangeEnd: function(swiper){
+                    swiperAnimate(swiper); //每个slide切换结束时也运行当前slide动画
+
+                }
+            })
+
+
+        });
+
+
+    }
+]);
+
