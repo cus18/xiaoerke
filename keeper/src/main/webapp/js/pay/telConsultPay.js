d2c196f7e2fc29e992e51687de2ffd82e7430458
王保伟 <565871712@qq.com>
2017-05-13 16:09:38 +0800

    window.location.href=

--- keeper/src/main/webapp/js/pay/telConsultPay.js
+++ keeper/src/main/webapp/js/pay/telConsultPay.js
@@ -1,3 +1,11 @@
+//页面初始化执行,用户初始化页面参数信息以及微信的支付接口
+var GetQueryString = function(name)
+{
+    var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
+    var r = window.location.search.substr(1).match(reg);
+    if(r!=null)return  unescape(r[2]); return null;
+}
+var payPrice = GetQueryString("payPrice");
 
 //页面初始化执行,用户初始化页面参数信息以及微信的支付接口
 var doRefresh = function(){
@@ -49,10 +57,10 @@ var doRefresh = function(){
 //确认支付
 function wechatPay (){
     $.ajax({
-        url:"account/user/nonRealTimeConsultPay",// 跳转到 action
+        url:"account/user/phoneConsultPay",// 跳转到 action
         async:true,
         type:'get',
-        data:{patientRegisterId:consultId,payPrice:parseFloat(nonRealPayPrice)*100},
+        data:{patientRegisterId:consultId,payPrice:parseFloat(payPrice)*100},
         cache:false,
         success:function(data) {
             var obj = eval('(' + data + ')');
@@ -77,7 +85,7 @@ function wechatPay (){
                 paySign:obj.paySign,  // 支付签名
                 success: function (res) {
                     if(res.errMsg == "chooseWXPay:ok" ) {
-                        window.location.href = "/titan/consultDoctorHome#/telConsultPaySuccess";
+                        window.location.href = "http://s201.xiaork.com/titan/consultDoctorHome#/telConsultPaySuccess";
                     }else{
                         alert("支付失败,请重新支付")
                     }
