44a972eef9f5c556649a34fa906cf3e5106cc2ec
王保伟 <565871712@qq.com>
2017-06-14 23:07:54 +0800

    window.location.href=

--- xiaoerke-web/src/main/java/com/cxqm/xiaoerke/modules/account/web/PayNotificationController.java
+++ xiaoerke-web/src/main/java/com/cxqm/xiaoerke/modules/account/web/PayNotificationController.java
@@ -313,7 +313,7 @@ public class PayNotificationController {
                 payRecord.setId((String) map.get("out_trade_no"));
                 payRecord.setStatus("success");
                 payRecord.setReceiveDate(new Date());
-                Map<String, Object> consultPhoneMap = insuranceService.getPayRecordById(payRecord.getOrderId());
+                Map<String, Object> consultPhoneMap = insuranceService.getPayRecordById(payRecord.getId());
 
                 ConsultPhoneRegisterServiceVo vo = consultPhonePatientService.selectByPrimaryKey((Integer) consultPhoneMap.get("order_id"));
                 CCPRestSDK.callback(vo.getPhoneNum(),vo.getPhoneNum(),
